1953b602c7abbc3c92301e4a48e73c8eeed11fc0
woodser <woodser@protonmail.com>
2017-04-11 01:18:29 -0400

    Compiling
    Former-commit-id: 87111533aa1c00b194f7be47a6f91e1597f2a48c

--- monero-wallet/src/wallet/MoneroResponse.java
+++ monero-wallet/src/wallet/MoneroResponse.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class MoneroResponse {
 
 	private String id;
-	private String jsonrpc
+	private String jsonrpc;
 	private String txKey;
 	private List<Integer> fees;
 	private List<String> txHashList;
