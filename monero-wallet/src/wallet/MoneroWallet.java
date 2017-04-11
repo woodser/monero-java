8578d1ec2a4000a0bc4bc23d551de112f0db4a70
woodser <woodser@protonmail.com>
2017-04-11 00:54:46 -0400

    Starting to build MoneroWallet interface.
    Former-commit-id: 9b9fc94a08e56f09d88a1d00894e1d83911cdd22

--- monero-wallet/src/wallet/MoneroWallet.java
+++ monero-wallet/src/wallet/MoneroWallet.java
@@ -2,7 +2,14 @@ package wallet;
 
 import org.jooq.types.UInteger;
 
+/**
+ * Defines a Monero wallet interface.
+ * 
+ * @author woodser
+ */
 public interface MoneroWallet {
 
 	public UInteger getBalance();
+	
+	public UInteger getUnlockedBalance();
 }
