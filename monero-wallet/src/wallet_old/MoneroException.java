84baf0377a234e4519e7e604813d341b15bbfb31
woodser <woodser@protonmail.com>
2017-04-12 07:51:25 -0400

    Removed old wallet code

--- monero-wallet/src/wallet_old/MoneroException.java
+++
@@ -1,9 +0,0 @@
-package wallet_old;
-
-public class MoneroException extends RuntimeException {
-
-	private static final long serialVersionUID = 6158997228066019103L;
-	
-	private int code;
-	private String message;
-}
