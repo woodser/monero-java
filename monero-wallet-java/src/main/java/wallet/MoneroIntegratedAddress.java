bf1bbcfe0be99f2954c2fb203b0e526adfa74d2a
woodser <woodser@protonmail.com>
2017-04-16 09:30:36 -0400

    First send transaction test implemented
    Former-commit-id: 236ed75bbfc91d6166e1a0ec1151697ed7f32e1e

--- monero-wallet-java/src/main/java/wallet/MoneroIntegratedAddress.java
+++ monero-wallet-java/src/main/java/wallet/MoneroIntegratedAddress.java
@@ -22,4 +22,28 @@ public class MoneroIntegratedAddress extends MoneroAddress {
   public String toString() {
     return integratedAddress;
   }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = super.hashCode();
+    result = prime * result + ((integratedAddress == null) ? 0 : integratedAddress.hashCode());
+    result = prime * result + ((paymentId == null) ? 0 : paymentId.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (!super.equals(obj)) return false;
+    if (getClass() != obj.getClass()) return false;
+    MoneroIntegratedAddress other = (MoneroIntegratedAddress) obj;
+    if (integratedAddress == null) {
+      if (other.integratedAddress != null) return false;
+    } else if (!integratedAddress.equals(other.integratedAddress)) return false;
+    if (paymentId == null) {
+      if (other.paymentId != null) return false;
+    } else if (!paymentId.equals(other.paymentId)) return false;
+    return true;
+  }
 }
