7fc48967c435ede1228e99851e8e07cd4ff136b3
woodser <woodser@protonmail.com>
2018-06-09 00:01:04 -0400

    migrate to new MoneroWallet
    Former-commit-id: db76a2a02697fd4583453c00fd087192bbf899c2

--- monero-wallet-java/src/main/java/service/MoneroWallet.java
+++ monero-wallet-java/src/main/java/service/MoneroWallet.java
@@ -1,10 +1,13 @@
 package service;
 
+import java.net.URI;
 import java.util.List;
 
 import api.MoneroAccount;
+import api.MoneroIntegratedAddress;
 import api.MoneroKeyImage;
 import api.MoneroTransaction;
+import api.MoneroUri;
 
 public interface MoneroWallet {
   
@@ -57,9 +60,114 @@ public interface MoneroWallet {
    * 
    * @return List<MoneroTransaction> are the resulting transactions from sweeping dust
    */
-  public List<MoneroTransaction> sweepDust();  // TODO: stopped here, go through other MoneroWallet.java
+  public List<MoneroTransaction> sweepDust();
+  
+  /**
+   * Set arbitrary string notes for transactions.
+   * 
+   * @param txIds identify the transactions to get notes for
+   * @param txNotes are the notes to set for transactions
+   */
+  public void setTxNotes(List<String> txIds, List<String> txNotes);
+  
+  /**
+   * Get arbitrary string notes for transactions.
+   * 
+   * @param txIds identify the transactions to get notes for
+   */
+  public void getTxNotes(List<String> txIds);
 
   public List<MoneroKeyImage> getKeyImages();
   
   public void importKeyImages(List<MoneroKeyImage> keyImages);
+  
+  /**
+   *  --------------------------- SERVICE UTILITIES ---------------------------
+   */
+  
+  /**
+   * Get a list of available languages for wallet seeds.
+   * 
+   * @return List<String> is a list of available languages
+   */
+  public List<String> getLanguages();
+  
+  /**
+   * Create a new wallet.
+   * 
+   * @param filename is the name of the wallet file to create
+   * @param password is the wallet password
+   * @param language is the wallet language
+   */
+  public void createWallet(String filename, String password, String language);
+  
+  /**
+   * Open a wallet.
+   * 
+   * @param filename is the name of the wallet file to open
+   * @param password is the wallet password
+   */
+  public void openWallet(String filename, String password);
+  
+  /**
+   * Sign a string.
+   * 
+   * @param data is the string to sign
+   * @return String is the signature
+   */
+  public String sign(String data);
+  
+  /**
+   * Verify a signature on a string.
+   * 
+   * @param data is the signed string
+   * @param address is the signing address
+   * @param signature is the signature
+   * @return true if the signature is good, false otherwise
+   */
+  public boolean verify(String data, String address, String signature);
+  
+  /**
+   * Convert a MoneroUri to a standard URI.
+   * 
+   * @param moneroUri is the MoneroUri to convert to a standard URI
+   * @return URI is the MoneroUri converted to a standard URI
+   */
+  public URI toUri(MoneroUri moneroUri);
+
+  /**
+   * Convert a standard URI to a Monero URI.
+   * 
+   * @param uri is the standard URI to convert
+   * @return MoneroUri is the URI converted to a Monero URI
+   */
+  public MoneroUri toMoneroUri(URI uri);
+  
+  /**
+   * Decodes an integrated address into its standard address and payment id components.
+   * 
+   * @param integratedAddress is a string representation of the integrated address
+   * @return MoneroIntegratedAddress contains the integrated address, standard address, and payment id
+   */
+  public MoneroIntegratedAddress decodeIntegratedAddress(String integratedAddress);
+  
+  /**
+   * Save the current state of the blockchain.
+   */
+  public void saveBlockchain();
+  
+  /**
+   * Rescan the blockchain.
+   */
+  public void rescanBlockchain();
+  
+  /**
+   * Rescan the blockchain for spent outputs.
+   */
+  public void rescanSpent();
+
+  /**
+   * Stop the wallet.
+   */
+  public void stopWallet();
 }
