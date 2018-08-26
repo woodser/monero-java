e654d847e13c86325ef2251321bd531746a5712e
woodser <woodser@protonmail.com>
2018-08-26 10:37:38 -0400

    prepping for first run

--- monero-wallet-java/src/test/java/utils/PrintBalances.java
+++ monero-wallet-java/src/test/java/utils/PrintBalances.java
@@ -1,6 +1,6 @@
 package utils;
 
-import service.MoneroAccount;
+import service.MoneroWallet;
 
 /**
  * Prints the balances of the wallet.
@@ -8,8 +8,8 @@ import service.MoneroAccount;
 public class PrintBalances {
 
   public static void main(String[] args) {
-    MoneroAccount wallet = TestUtils.getWallet();
-    System.out.println("Balance: " + wallet.getBalance());
-    System.out.println("Unlocked: " + wallet.getUnlockedBalance());
+    MoneroWallet wallet = TestUtils.getWallet();
+    System.out.println("Balance: " + wallet.getBalance(0));
+    System.out.println("Unlocked: " + wallet.getUnlockedBalance(0));
   }
 }
