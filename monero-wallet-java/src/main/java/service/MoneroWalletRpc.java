5cc1640336ad4150eb8f1092de297f81b444098b
woodser <woodser@protonmail.com>
2018-08-26 12:27:31 -0400

    MoneroWalletRpc uses MoneroRpc to handle RPC interactions
    Former-commit-id: fde38fbbcd345d4db9796d860fd6bf8300da07ce

--- monero-wallet-java/src/main/java/service/MoneroWalletRpc.java
+++ monero-wallet-java/src/main/java/service/MoneroWalletRpc.java
@@ -9,17 +9,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -35,17 +24,14 @@ import model.MoneroKeyImage;
 import model.MoneroPayment;
 import model.MoneroSubaddress;
 import model.MoneroTx;
+import model.MoneroTx.MoneroTxType;
 import model.MoneroTxConfig;
 import model.MoneroTxFilter;
 import model.MoneroUri;
-import model.MoneroTx.MoneroTxType;
-import service.rpc.MoneroRpcException;
+import service.rpc.MoneroRpc;
 import service.rpc.MoneroWalletRpcOld;
-import types.HttpException;
 import types.Pair;
-import utils.JsonUtils;
 import utils.MoneroUtils;
-import utils.StreamUtils;
 
 /**
  * Implements a Monero Wallet using a monero-wallet-rpc backend.
@@ -63,55 +49,32 @@ public class MoneroWalletRpc extends MoneroWalletDefault {
     MAPPER.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false);
     MAPPER.configure(DeserializationFeature.USE_BIG_INTEGER_FOR_INTS, true);
   }
-
-  // instance variables
-  private String rpcHost;
-  private int rpcPort;
-  private URI rpcUri;
-  private HttpClient client;
+  
+  private MoneroRpc rpc;  // handles rpc interactions
 
   public MoneroWalletRpc(String endpoint) {
-    this(parseUri(endpoint));
+    rpc = new MoneroRpc(endpoint);
   }
 
   public MoneroWalletRpc(URI rpcUri) {
-    this.rpcUri = rpcUri;
-    this.rpcHost = rpcUri.getHost();
-    this.rpcPort = rpcUri.getPort();
-    this.client = HttpClients.createDefault();
+    rpc = new MoneroRpc(rpcUri);
   }
 
   public MoneroWalletRpc(String rpcHost, int rpcPort) throws URISyntaxException {
-    this.rpcHost = rpcHost;
-    this.rpcPort = rpcPort;
-    this.rpcUri = new URI("http", null, rpcHost, rpcPort, "/json_rpc", null, null);
-    this.client = HttpClients.createDefault();
+    rpc = new MoneroRpc(rpcHost, rpcPort);
   }
 
   public MoneroWalletRpc(String rpcHost, int rpcPort, String username, String password) throws URISyntaxException {
-    this.rpcHost = rpcHost;
-    this.rpcPort = rpcPort;
-    this.rpcUri = new URI("http", null, rpcHost, rpcPort, "/json_rpc", null, null);
-    CredentialsProvider creds = new BasicCredentialsProvider();
-    creds.setCredentials(new AuthScope(rpcUri.getHost(), rpcUri.getPort()), new UsernamePasswordCredentials(username, password));
-    this.client = HttpClients.custom().setDefaultCredentialsProvider(creds).build();
+    rpc = new MoneroRpc(rpcHost, rpcPort, username, password);
   }
-
-  public String getRpcHost() {
-    return rpcHost;
-  }
-
-  public int getRpcPort() {
-    return rpcPort;
-  }
-
-  public URI getRpcUri() {
-    return rpcUri;
+  
+  public MoneroRpc getRpc() {
+    return rpc;
   }
 
   @Override
   public int getHeight() {
-    Map<String, Object> respMap = sendRpcRequest("getheight", null);
+    Map<String, Object> respMap = rpc.sendRpcRequest("getheight", null);
     @SuppressWarnings("unchecked")
     Map<String, Object> resultMap = (Map<String, Object>) respMap.get("result");
     return ((BigInteger) resultMap.get("height")).intValue();
@@ -326,7 +289,7 @@ public class MoneroWalletRpc extends MoneroWalletDefault {
   
   @SuppressWarnings("unchecked")
   private Pair<BigInteger, BigInteger> getBalances() {
-    Map<String, Object> respMap = sendRpcRequest("getbalance", null);
+    Map<String, Object> respMap = rpc.sendRpcRequest("getbalance", null);
     Map<String, Object> resultMap = (Map<String, Object>) respMap.get("result");
     return new Pair<BigInteger, BigInteger>((BigInteger) resultMap.get("balance"), (BigInteger) resultMap.get("unlocked_balance"));
   }
@@ -376,50 +339,6 @@ public class MoneroWalletRpc extends MoneroWalletDefault {
     return tx;
   }
   
-  /**
-   * Sends a request to the RPC API.
-   * 
-   * @param method specifies the method to request
-   * @param params specifies input parameters
-   * @return Map<String, Object> is the RPC API response as a map
-   */
-  private Map<String, Object> sendRpcRequest(String method, Map<String, Object> params) {
-
-    // send http request
-    try {
-
-      // build request body
-      Map<String, Object> body = new HashMap<String, Object>();
-      body.put("jsonrpc", "2.0");
-      body.put("id", "0");
-      body.put("method", method);
-      if (params != null) body.put("params", params);
-      LOGGER.debug("Sending method '" + method + "' with body: " + JsonUtils.serialize(body));
-
-      // send http request and validate response
-      HttpPost post = new HttpPost(rpcUri);
-      HttpEntity entity = new StringEntity(JsonUtils.serialize(body));
-      post.setEntity(entity);
-      HttpResponse resp = client.execute(post);
-      validateHttpResponse(resp);
-
-      // deserialize response
-      Map<String, Object> respMap = JsonUtils.toMap(MAPPER, StreamUtils.streamToString(resp.getEntity().getContent()));
-      LOGGER.debug("Received response to method '" + method + "': " + JsonUtils.serialize(respMap));
-      EntityUtils.consume(resp.getEntity());
-
-      // check RPC response for errors
-      validateRpcResponse(respMap, body);
-      return respMap;
-    } catch (HttpException e1) {
-      throw e1;
-    } catch (MoneroRpcException e2) {
-      throw e2;
-    } catch (Exception e3) {
-      throw new MoneroException(e3);
-    }
-  }
-  
   // ------------------------------ STATIC UTILITIES --------------------------
 
   private static void addTx(Map<MoneroTxType, Map<String, MoneroTx>> txTypeMap, MoneroTx tx) {
@@ -446,35 +365,5 @@ public class MoneroWalletRpc extends MoneroWalletDefault {
     else if (type.equalsIgnoreCase("failed")) return MoneroTxType.FAILED;
     else if (type.equalsIgnoreCase("pool")) return MoneroTxType.MEMPOOL;
     throw new MoneroException("Unrecognized transaction type: " + type);
-  }
-
-  private static void validateHttpResponse(HttpResponse resp) {
-    int code = resp.getStatusLine().getStatusCode();
-    if (code < 200 || code > 299) {
-      String content = null;
-      try {
-        content = StreamUtils.streamToString(resp.getEntity().getContent());
-      } catch (Exception e) {
-        // could not get content
-      }
-      throw new HttpException(code, resp.getStatusLine().getReasonPhrase() + (content != null ? (": " + content) : ""));
-    }
-  }
-
-  @SuppressWarnings("unchecked")
-  private static void validateRpcResponse(Map<String, Object> respMap, Map<String, Object> requestBody) {
-    Map<String, Object> error = (Map<String, Object>) respMap.get("error");
-    if (error == null) return;
-    int code = ((BigInteger) error.get("code")).intValue();
-    String message = (String) error.get("message");
-    throw new MoneroRpcException(code, message, requestBody);
-  }
-  
-  private static URI parseUri(String endpoint) {
-    try {
-      return new URI(endpoint);
-    } catch (Exception e) {
-      throw new MoneroException(e);
-    }
   }
 }
