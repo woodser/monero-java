dc738f40869633ecd35e48a1d024cfa056e03d96
woodser <woodser@protonmail.com>
2018-09-04 13:26:13 -0400

    basic tests and implementation for send, sendSplit, and sweepAll

--- monero-wallet-java/src/test/java/test/TestMoneroWalletSends.java
+++ monero-wallet-java/src/test/java/test/TestMoneroWalletSends.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
@@ -15,6 +16,7 @@ import org.junit.Test;
 import model.MoneroAddress;
 import model.MoneroPayment;
 import model.MoneroTx;
+import model.MoneroTxConfig;
 import utils.TestUtils;
 import wallet.MoneroWallet;
 
@@ -22,6 +24,8 @@ import wallet.MoneroWallet;
  * Tests sending transactions using a Monero wallet.
  * 
  * These tests are separated because they rely on a balance and initiate transactions on the blockchain.
+ * 
+ * TODO: support / test send, sendSplit, and sweepAll from specific accounts and subaddresses
  */
 public class TestMoneroWalletSends {
   
@@ -40,15 +44,12 @@ public class TestMoneroWalletSends {
     }
   }
   
-  // TODO: test more than account 0
   @Test
-  public void testSend0() {
+  public void testSend() {
     
     // get balance before
     BigInteger balanceBefore = wallet.getBalance(0);
     MoneroWallet wallet = TestUtils.getWallet();
-    System.out.println("Balance: " + wallet.getBalance(0));
-    System.out.println("Unlocked: " + wallet.getUnlockedBalance(0));
     BigInteger unlockedBalanceBefore = wallet.getUnlockedBalance(0);
     assertTrue("Wallet is empty; load '" + TestUtils.WALLET_NAME_1 + "' with XMR in order to test sending", balanceBefore.longValue() > 0);
     assertTrue("Wallet is waiting on unlocked funds", unlockedBalanceBefore.longValue() > 0);
@@ -56,6 +57,8 @@ public class TestMoneroWalletSends {
     // send to self
     MoneroAddress address = wallet.getSubaddress(0, 0).getAddress();
     BigInteger sendAmount = unlockedBalanceBefore.divide(BigInteger.valueOf(SEND_DIVISOR));
+    System.out.println("Balance: " + wallet.getBalance(0));
+    System.out.println("Unlocked: " + wallet.getUnlockedBalance(0));
     System.out.println("Send amount: " + sendAmount);
     MoneroTx tx = wallet.send(address.getStandardAddress(), null, sendAmount, MIXIN);
     
@@ -88,7 +91,45 @@ public class TestMoneroWalletSends {
 
   @Test
   public void testSendSplit() {
-    fail("Not yet implemented");
+    
+    // get balance and address
+    BigInteger balanceBefore = wallet.getBalance(0);
+    BigInteger unlockedBalanceBefore = wallet.getUnlockedBalance(0);
+    MoneroAddress address = wallet.getPrimaryAddress();
+    assertTrue("Wallet is empty; load '" + TestUtils.WALLET_NAME_1 + "' with XMR in order to test sending", balanceBefore.longValue() > 0);
+    assertTrue("Wallet is waiting on unlocked funds", unlockedBalanceBefore.longValue() > 0);
+    
+    // create payments to send
+    int numPayments = 3;
+    BigInteger sendAmount = unlockedBalanceBefore.divide(BigInteger.valueOf(numPayments + SEND_DIVISOR));
+    List<MoneroPayment> payments = new ArrayList<MoneroPayment>();
+    for (int i = 0; i < numPayments; i++) {
+      payments.add(new MoneroPayment(address, sendAmount));
+    }
+    
+    // send payments
+    MoneroTxConfig config = new MoneroTxConfig();
+    config.setDestinations(payments);
+    config.setMixin(MIXIN);
+    List<MoneroTx> txs = wallet.sendSplit(config);
+    
+    // test transactions
+    for (MoneroTx tx : txs) {
+      assertNull(tx.getPayments());
+      assertTrue(tx.getFee().longValue() > 0);
+      assertEquals(MIXIN, tx.getMixin());
+      assertNull(tx.getKey());
+      assertNotNull(tx.getHash());
+      assertNull(tx.getHeight());
+      assertNull(tx.getSize());
+      assertNull(tx.getType());
+      assertNull(tx.getHeight());
+      assertEquals((Integer) 0, tx.getUnlockTime());
+    }
+    
+    // test wallet balance
+    assertTrue(wallet.getBalance(0).longValue() < balanceBefore.longValue());
+    assertTrue(wallet.getUnlockedBalance(0).longValue() < unlockedBalanceBefore.longValue());
   }
 
   @Test
