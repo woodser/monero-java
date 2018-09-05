af7290ce311557a3b780278a6450e9c8d169c667
woodser <woodser@protonmail.com>
2018-09-05 15:12:44 -0400

    broke out account and subaddress creation; ready for implementation
    Former-commit-id: 8b34bdc38303c4f74d04b49fc830c047df9757aa

---
+++ monero-wallet-java/src/test/java/test/TestMoneroWalletCreateAccountsAndSubaddresses.java
@@ -0,0 +1,136 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.math.BigInteger;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import model.MoneroAccount;
+import model.MoneroAddress;
+import model.MoneroAddressBookEntry;
+import model.MoneroException;
+import model.MoneroIntegratedAddress;
+import model.MoneroKeyImage;
+import model.MoneroOutput;
+import model.MoneroPayment;
+import model.MoneroSubaddress;
+import model.MoneroTx;
+import model.MoneroTx.MoneroTxType;
+import model.MoneroTxFilter;
+import model.MoneroUri;
+import utils.MoneroUtils;
+import utils.TestUtils;
+import wallet.MoneroWallet;
+import wallet.rpc.MoneroRpcException;
+
+/**
+ * Tests a Monero wallet's account and subaddress creation function.
+ */
+public class TestMoneroWalletCreateAccountsAndSubaddresses {
+  
+  private MoneroWallet wallet;
+
+  @Before
+  public void setup() throws Exception {
+    wallet = TestUtils.getWallet();
+  }
+
+  @Test
+  public void testCreateAccount() {
+    
+    // test creation with null tag
+    List<MoneroAccount> accountsBefore = wallet.getAccounts();
+    MoneroAccount createdAccount = wallet.createAccount(null);
+    testAccount(createdAccount);
+    assertNull(createdAccount.getLabel());
+    assertTrue(accountsBefore.size() == wallet.getAccounts().size() - 1);
+    
+    // test creation with tag
+    accountsBefore = wallet.getAccounts();
+    String tag = UUID.randomUUID().toString();
+    createdAccount = wallet.createAccount(tag);
+    assertEquals(tag, createdAccount.getLabel());
+    testAccount(createdAccount);
+    assertTrue(accountsBefore.size() == wallet.getAccounts().size() - 1);
+    
+    // test querying by created tag
+    List<MoneroAccount> accountsByTag = wallet.getAccounts(tag);
+    assertEquals(1, accountsByTag.size());
+    assertEquals(createdAccount, accountsByTag.get(0));
+    
+    // create another account with the same tag
+    createdAccount = wallet.createAccount(tag);
+    assertEquals(tag, createdAccount.getLabel());
+    accountsByTag = wallet.getAccounts(tag);
+    assertEquals(2, accountsByTag.size());
+    assertEquals(createdAccount, accountsByTag.get(1));
+  }
+
+  @Test
+  public void testCreateSubaddress() {
+    
+    // create subaddress with no label
+    List<MoneroSubaddress> subaddresses = wallet.getSubaddresses(0);
+    MoneroSubaddress subaddress = wallet.createSubaddress(0, null);
+    assertNull(subaddress.getLabel());
+    testSubaddress(subaddress);
+    List<MoneroSubaddress> subaddressesNew = wallet.getSubaddresses(0);
+    assertEquals(subaddresses.size(), subaddressesNew.size() - 1);
+    assertEquals(subaddressesNew.get(subaddressesNew.size() - 1), subaddress);
+    
+    // create subaddress with label
+    subaddresses = wallet.getSubaddresses(0);
+    String uuid = UUID.randomUUID().toString();
+    subaddress = wallet.createSubaddress(0, uuid);
+    assertEquals(subaddress.getLabel(), uuid);
+    testSubaddress(subaddress);
+    subaddressesNew = wallet.getSubaddresses(0);
+    assertEquals(subaddresses.size(), subaddressesNew.size() - 1);
+    assertEquals(subaddressesNew.get(subaddressesNew.size() - 1), subaddress);
+  }
+
+  
+  
+  // --------------------------------- PRIVATE --------------------------------
+  
+  private static void testAccount(MoneroAccount account) {
+    assertTrue(account.getIndex() >= 0);
+    assertNotNull(account.getPrimaryAddress());
+    assertTrue(account.getBalance().doubleValue() >= 0);
+    assertTrue(account.getUnlockedBalance().doubleValue() >= 0);
+    assertFalse(account.isMultisigImportNeeded());
+    if (account.getSubaddresses() != null) {
+      for (int i = 0; i < account.getSubaddresses().size(); i++) {
+        testSubaddress(account.getSubaddresses().get(i));
+      }
+    }
+  }
+  
+  private static void testSubaddress(MoneroSubaddress subaddress) {
+    assertTrue(subaddress.getIndex() >= 0);
+    assertNotNull(subaddress.getAddress());
+    assertTrue(subaddress.getBalance().doubleValue() >= 0);
+    assertTrue(subaddress.getUnlockedBalance().doubleValue() >= 0);
+    assertTrue(subaddress.getNumUnspentOutputs() >= 0);
+    assertFalse(subaddress.isMultisigImportNeeded());
+    if (subaddress.getBalance().doubleValue() >= 0) assertTrue(subaddress.isUsed());
+  }
+}
