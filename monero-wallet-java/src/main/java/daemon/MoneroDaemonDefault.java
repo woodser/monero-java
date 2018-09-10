daa2ac48ae637436aa3c705b1d4ccc7e162690ed
woodser <woodser@protonmail.com>
2018-09-10 11:06:14 -0400

    define getSyncInfo

--- monero-wallet-java/src/main/java/daemon/MoneroDaemonDefault.java
+++ monero-wallet-java/src/main/java/daemon/MoneroDaemonDefault.java
@@ -1,6 +1,10 @@
 package daemon;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import daemon.model.MoneroBan;
+import daemon.model.MoneroDaemonResponseInfo;
 
 /**
  * Default Monero daemon implementation.
@@ -10,5 +14,12 @@ public abstract class MoneroDaemonDefault implements MoneroDaemon {
   @Override
   public String setBan(MoneroBan ban) {
     throw new RuntimeException("Not implemented");
+  }
+  
+  @Override
+  public MoneroDaemonResponseInfo relayTx(String txId) {
+    Collection<String> txIds = new ArrayList<>();
+    txIds.add(txId);
+    return relayTxs(txIds);
   }
 }
---
+++ monero-wallet-java/src/main/java/daemon/model/MoneroDaemonConnectionSpan.java
@@ -0,0 +1,15 @@
+package daemon.model;
+
+/**
+ * Monero daemon connection span.
+ */
+public class MoneroDaemonConnectionSpan {
+
+  private String connectionId;
+  private Integer numBlocks;
+  private Integer connectionRate;
+  private String remoteAddress;
+  private Integer size;
+  private Integer speed;
+  private Integer startBlockHeight;
+}
