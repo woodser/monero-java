6c2af2b43509f1ff1d2bc9132f5a92af51384f00
woodser <woodser@protonmail.com>
2018-09-10 09:34:12 -0400

    defined hard fork info

---
+++ monero-wallet-java/src/main/java/daemon/model/MoneroHardForkInfo.java
@@ -0,0 +1,16 @@
+package daemon.model;
+
+/**
+ * Monero hard fork info.
+ */
+public class MoneroHardForkInfo {
+
+  private Integer earliestHeight;
+  private Boolean isEnabled;
+  private Integer state;
+  private Integer threshold;
+  private Integer version;
+  private Integer votes;
+  private Integer voting;
+  private Integer window;
+}
