792f5a6b115a0f5d5afd9b7e6475a52de30aea37
woodser <woodser@protonmail.com>
2018-09-12 17:23:06 -0400

    tst get sync info
    Former-commit-id: aaff09861abcab26dc3829805fb9ff5317bc35b3

--- monero-wallet-java/src/main/java/daemon/model/MoneroDaemonConnectionSpan.java
+++ monero-wallet-java/src/main/java/daemon/model/MoneroDaemonConnectionSpan.java
@@ -3,7 +3,7 @@ package daemon.model;
 /**
  * Monero daemon connection span.
  */
-public class MoneroDaemonConnectionSpan {
+public class MoneroDaemonConnectionSpan extends MoneroDaemonModel {
 
   private String connectionId;
   private Integer numBlocks;
@@ -12,4 +12,60 @@ public class MoneroDaemonConnectionSpan {
   private Integer size;
   private Integer speed;
   private Integer startBlockHeight;
+
+  public String getConnectionId() {
+    return connectionId;
+  }
+
+  public void setConnectionId(String connectionId) {
+    this.connectionId = connectionId;
+  }
+
+  public Integer getNumBlocks() {
+    return numBlocks;
+  }
+
+  public void setNumBlocks(Integer numBlocks) {
+    this.numBlocks = numBlocks;
+  }
+
+  public Integer getConnectionRate() {
+    return connectionRate;
+  }
+
+  public void setConnectionRate(Integer connectionRate) {
+    this.connectionRate = connectionRate;
+  }
+
+  public String getRemoteAddress() {
+    return remoteAddress;
+  }
+
+  public void setRemoteAddress(String remoteAddress) {
+    this.remoteAddress = remoteAddress;
+  }
+
+  public Integer getSize() {
+    return size;
+  }
+
+  public void setSize(Integer size) {
+    this.size = size;
+  }
+
+  public Integer getSpeed() {
+    return speed;
+  }
+
+  public void setSpeed(Integer speed) {
+    this.speed = speed;
+  }
+
+  public Integer getStartBlockHeight() {
+    return startBlockHeight;
+  }
+
+  public void setStartBlockHeight(Integer startBlockHeight) {
+    this.startBlockHeight = startBlockHeight;
+  }
 }
