423fe79ccbfb9588ae4e47d9b8795f932a0e1c62
woodser <woodser@protonmail.com>
2018-09-12 17:57:28 -0400

    test distribution, histogram, and coinbase tx sum
    Former-commit-id: 2a8f4f9fbc107232ce54da7c9bf33ade3be8670f

--- monero-wallet-java/src/main/java/daemon/model/MoneroOutputHistogramEntry.java
+++ monero-wallet-java/src/main/java/daemon/model/MoneroOutputHistogramEntry.java
@@ -11,4 +11,36 @@ public class MoneroOutputHistogramEntry extends MoneroDaemonModel {
   private Integer numTotalInstances;
   private Integer numUnlockedInstances;
   private Integer numRecentInstances;
+  
+  public BigInteger getAmount() {
+    return amount;
+  }
+  
+  public void setAmount(BigInteger amount) {
+    this.amount = amount;
+  }
+  
+  public Integer getNumTotalInstances() {
+    return numTotalInstances;
+  }
+  
+  public void setNumTotalInstances(Integer numTotalInstances) {
+    this.numTotalInstances = numTotalInstances;
+  }
+  
+  public Integer getNumUnlockedInstances() {
+    return numUnlockedInstances;
+  }
+  
+  public void setNumUnlockedInstances(Integer numUnlockedInstances) {
+    this.numUnlockedInstances = numUnlockedInstances;
+  }
+  
+  public Integer getNumRecentInstances() {
+    return numRecentInstances;
+  }
+  
+  public void setNumRecentInstances(Integer numRecentInstances) {
+    this.numRecentInstances = numRecentInstances;
+  }
 }
