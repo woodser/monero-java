7ecc58b781b51bfe65a7889f9b28b5600f6c01f9
woodser <woodser@protonmail.com>
2018-09-12 17:38:48 -0400

    test setBans()

--- monero-wallet-java/src/main/java/daemon/model/MoneroBan.java
+++ monero-wallet-java/src/main/java/daemon/model/MoneroBan.java
@@ -9,4 +9,36 @@ public class MoneroBan extends MoneroDaemonModel {
   private Integer ip;   // integer formatted IP
   private Boolean isBanned;
   private Integer seconds;
+  
+  public String getHost() {
+    return host;
+  }
+  
+  public void setHost(String host) {
+    this.host = host;
+  }
+  
+  public Integer getIp() {
+    return ip;
+  }
+  
+  public void setIp(Integer ip) {
+    this.ip = ip;
+  }
+  
+  public Boolean getIsBanned() {
+    return isBanned;
+  }
+  
+  public void setIsBanned(Boolean isBanned) {
+    this.isBanned = isBanned;
+  }
+  
+  public Integer getSeconds() {
+    return seconds;
+  }
+  
+  public void setSeconds(Integer seconds) {
+    this.seconds = seconds;
+  }
 }
