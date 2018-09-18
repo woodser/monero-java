d3dc845c003627e6152aed8d4dde83c9e32b16fb
woodser <woodser@protonmail.com>
2018-09-18 11:49:49 -0400

    PrintAddresses prints CSV

--- monero-wallet-java/src/test/java/utils/PrintAddresses.java
+++ monero-wallet-java/src/test/java/utils/PrintAddresses.java
@@ -1,7 +1,9 @@
 package utils;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import common.types.Pair;
 import monero.wallet.MoneroWallet;
 import monero.wallet.model.MoneroAccount;
 import monero.wallet.model.MoneroSubaddress;
@@ -12,14 +14,63 @@ import monero.wallet.model.MoneroSubaddress;
 public class PrintAddresses {
 
   public static void main(String[] args) {
+    
+    // collect info about subaddresses
+    List<Pair<String, List<Object>>> pairs = new ArrayList<Pair<String, List<Object>>>();
     MoneroWallet wallet = TestUtils.getWallet();
-    System.out.println("Primary address: " + wallet.getPrimaryAddress());
     List<MoneroAccount> accounts = wallet.getAccounts();
     for (MoneroAccount account : accounts) {
+      add(pairs, "ACCOUNT", account.getIndex());
+      add(pairs, "SUBADDRESS", 0);
+      add(pairs, "LABEL", account.getLabel());
+      add(pairs, "ADDRESS", account.getPrimaryAddress());
+      add(pairs, "BALANCE", account.getBalance());
+      add(pairs, "UNLOCKED", account.getUnlockedBalance());
       List<MoneroSubaddress> subaddresses = wallet.getSubaddresses(account.getIndex());
       for (MoneroSubaddress subaddress : subaddresses) {
-        System.out.println("[" + account.getIndex() + ", " + subaddress.getIndex() + "] " + subaddress.getAddress() + " " + subaddress.getBalance() + " balance " + subaddress.getUnlockedBalance() + " unlocked");
+        add(pairs, "ACCOUNT", account.getIndex());
+        add(pairs, "SUBADDRESS", subaddress.getIndex());
+        add(pairs, "LABEL", subaddress.getLabel());
+        add(pairs, "ADDRESS", subaddress.getAddress());
+        add(pairs, "BALANCE", subaddress.getBalance());
+        add(pairs, "UNLOCKED", subaddress.getUnlockedBalance());
       }
     }
+    
+    // convert info to csv
+    System.out.println(pairsToCsv(pairs));
+  }
+  
+  private static void add(List<Pair<String, List<Object>>> pairs, String header, Object value) {
+    Pair<String, List<Object>> pair = null;
+    for (Pair<String, List<Object>> aPair : pairs) {
+      if (aPair.getFirst().equals(header)) {
+        pair = aPair;
+        break;
+      }
+    }
+    if (pair == null) {
+      List<Object> vals = new ArrayList<Object>();
+      pair = new Pair<String, List<Object>>(header, vals);
+      pairs.add(pair);
+    }
+    pair.getSecond().add(value);
+  }
+  
+  private static String pairsToCsv(List<Pair<String, List<Object>>> pairs) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < pairs.size(); i++) {
+      sb.append(pairs.get(i).getFirst());
+      if (i < pairs.size() - 1) sb.append(',');
+      else sb.append('\n');
+    }
+    for (int i = 0; i < pairs.get(0).getSecond().size(); i++) {
+      for (int j = 0; j < pairs.size(); j++) {
+        sb.append(pairs.get(j).getSecond().get(i));
+        if (j < pairs.size() - 1) sb.append(',');
+        else sb.append('\n');
+      }
+    }
+    return sb.toString();
   }
 }
