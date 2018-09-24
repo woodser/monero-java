5d9f5c1ff5edca840f00e828cc3406c8391f9253
woodser <woodser@protonmail.com>
2018-09-24 10:17:28 -0400

    don't query get_bulk_payments which also doesn't return txs to 0

--- monero-wallet-java/src/test/java/utils/PrintTransactions.java
+++ monero-wallet-java/src/test/java/utils/PrintTransactions.java
@@ -1,5 +1,6 @@
 package utils;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -16,17 +17,15 @@ public class PrintTransactions {
   public static void main(String[] args) {
     MoneroWallet wallet = TestUtils.getWallet();
     MoneroTxFilter filter = new MoneroTxFilter();
-    //filter.setTxIds(Arrays.asList("0b93b598552f843e6ada0e2dc0dd9f8ed47d1228f0092d8728c8b8c30c1bf5d5"));
-    filter.setIncoming(false);
-    //filter.setAccountIndex(1);
+    filter.setTxIds(Arrays.asList("409778b3e5abbbeb3b35c9b532001e8b1c84b8743c40728219eeeb95c1c283bf"));
+    filter.setOutgoing(false);
+    filter.setIncoming(true);
+    //filter.setAccountIndex(0);
     Set<String> ids = new HashSet<String>();
     List<MoneroTx> txs = wallet.getTxs(filter);
     for (MoneroTx tx : txs) {
       ids.add(tx.getId());
       System.out.println(tx);
     }
-    
-    System.out.println(ids.size());
-    System.out.println(txs.size());
   }
 }
