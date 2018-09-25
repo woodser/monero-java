573811df3c18f4852d96cb17df54e1ae96fc89d8
woodser <woodser@protonmail.com>
2018-09-24 21:24:57 -0400

    use new api
    Former-commit-id: 1ad214ad9522781694638b0f82683bff3d346a25

--- monero-wallet-java/src/test/java/utils/PrintBalances.java
+++ monero-wallet-java/src/test/java/utils/PrintBalances.java
@@ -22,7 +22,7 @@ public class PrintBalances {
     // collect info about subaddresses
     List<Pair<String, List<Object>>> pairs = new ArrayList<Pair<String, List<Object>>>();
     MoneroWallet wallet = TestUtils.getWallet();
-    List<MoneroAccount> accounts = wallet.getAccounts();
+    List<MoneroAccount> accounts = wallet.getAccounts(true);
     for (MoneroAccount account : accounts) {
       add(pairs, "ACCOUNT", account.getIndex());
       add(pairs, "SUBADDRESS", "");
@@ -30,8 +30,7 @@ public class PrintBalances {
       add(pairs, "ADDRESS", "");
       add(pairs, "BALANCE", account.getBalance());
       add(pairs, "UNLOCKED", account.getUnlockedBalance());
-      List<MoneroSubaddress> subaddresses = wallet.getSubaddresses(account.getIndex());
-      for (MoneroSubaddress subaddress : subaddresses) {
+      for (MoneroSubaddress subaddress : account.getSubaddresses()) {
         add(pairs, "ACCOUNT", account.getIndex());
         add(pairs, "SUBADDRESS", subaddress.getIndex());
         add(pairs, "LABEL", subaddress.getLabel());
