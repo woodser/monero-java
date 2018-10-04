b1fb484930457685610343d17679cd134424393f
woodser <woodser@protonmail.com>
2018-10-04 16:48:05 -0400

    improve send test to reduce by fee per src subaddress
    Former-commit-id: d8bef35d20b172754497267ad8e6cd75cb70fc4c

--- src/test/java/utils/TestUtils.java
+++ src/test/java/utils/TestUtils.java
@@ -50,7 +50,7 @@ public class TestUtils {
   
   // test constants
   public static final Integer MIXIN = 6;
-  public static final BigInteger MAX_FEE = BigInteger.valueOf(8500000).multiply(BigInteger.valueOf(10000));
+  public static final BigInteger MAX_FEE = BigInteger.valueOf(7500000).multiply(BigInteger.valueOf(10000));
   
   // logger configuration
   public static final Logger LOGGER = Logger.getLogger(TestUtils.class);
