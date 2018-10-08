59482da5cde9fdd7a19092d6a3c13d8a97191c29
woodser <woodser@protonmail.com>
2018-10-08 16:47:28 -0400

    defined wallet get/set note and txkey, tx proof, spend proof, reserve proof
    Former-commit-id: a7660096b83ced02eb363cb5bd64828555e18fd5

---
+++ src/main/java/monero/wallet/model/MoneroTxCheck.java
@@ -0,0 +1,46 @@
+package monero.wallet.model;
+
+import java.math.BigInteger;
+
+/**
+ * Results from checking a transaction key or proof.
+ */
+public class MoneroTxCheck {
+
+  public Boolean isGood;
+  public Boolean isInPool;
+  public Integer numConfirmations;
+  public BigInteger amountReceived;
+  
+  public Boolean getIsGood() {
+    return isGood;
+  }
+  
+  public void setIsGood(Boolean isGood) {
+    this.isGood = isGood;
+  }
+  
+  public Boolean getIsInPool() {
+    return isInPool;
+  }
+  
+  public void setIsInPool(Boolean isInPool) {
+    this.isInPool = isInPool;
+  }
+  
+  public Integer getNumConfirmations() {
+    return numConfirmations;
+  }
+  
+  public void setNumConfirmations(Integer numConfirmations) {
+    this.numConfirmations = numConfirmations;
+  }
+  
+  public BigInteger getAmountReceived() {
+    return amountReceived;
+  }
+  
+  public void setAmountReceived(BigInteger amountReceived) {
+    this.amountReceived = amountReceived;
+  }
+}
