9a9250b76cb25c2027b63e7901e3afd05726a1ad
woodser <woodser@protonmail.com>
2018-10-09 14:33:50 -0400

    failed tx id

--- src/test/java/utils/PrintTransactions.java
+++ src/test/java/utils/PrintTransactions.java
@@ -19,11 +19,12 @@ public class PrintTransactions {
 //    filter.setOutgoing(false);
 //    filter.setIncoming(false);
     Set<String> ids = new HashSet<String>();
-    ids.add("6f081caf6508e9c47f2fabf6f32db0f177a25920eedabc2ca1557450998c214d");
+    ids.add("727de264a4a3278b3bab5cf6c4ccfab2f595543d70da907e924025c3082882ee");
     filter.setTxIds(ids);
     List<MoneroTx> txs = wallet.getTxs(filter);
     for (MoneroTx tx : txs) {
       System.out.println(tx);
+      //TestUtils.testGetTx(tx, hasOutgoingPayments, wallet);
     }
   }
 }
