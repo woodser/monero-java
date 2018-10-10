3e7c5f8ba060a77e29c8ef0cf99409fd29e255e7
woodser <woodser@protonmail.com>
2018-10-10 16:09:57 -0400

    TODO (monero-wall-rpc) this fails (confirmations is 0) for (at least one) transaction that has 1 confirmation on testCheckTxKey()
    Former-commit-id: 51f2094514563816f8f97952a2125f1d34dbfbfc

--- src/test/java/utils/PrintTransactions.java
+++ src/test/java/utils/PrintTransactions.java
@@ -19,7 +19,7 @@ public class PrintTransactions {
 //    filter.setOutgoing(false);
     filter.setIncoming(false);
     Set<String> ids = new HashSet<String>();
-    ids.add("e9699640fb3253f21c6a3b247b13d713b3dd5f89f3c2ab8b771748c4d562e406");
+    ids.add("d37bd0ef59f5dd40249b4aab6a23ec0fbb4432da68cf7a496212fc78a25afcba");
     filter.setTxIds(ids);
     List<MoneroTx> txs = wallet.getTxs(filter);
     for (MoneroTx tx : txs) {
