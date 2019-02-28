f8c2d62bdb3f14dd89a0586593b19855315bab37
woodser <woodser@protonmail.com>
2019-02-28 14:18:43 -0500

    ported daemon testTx()

--- src/test/java/test/TestMoneroDaemon.java
+++ src/test/java/test/TestMoneroDaemon.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import java.math.BigInteger;
 import java.util.List;
 
 import org.junit.Before;
@@ -16,8 +17,10 @@ import monero.daemon.MoneroDaemon;
 import monero.daemon.model.MoneroBlock;
 import monero.daemon.model.MoneroBlockHeader;
 import monero.daemon.model.MoneroBlockTemplate;
+import monero.daemon.model.MoneroOutput;
 import monero.daemon.model.MoneroTx;
 import monero.wallet.MoneroWallet;
+import monero.wallet.model.MoneroTransfer;
 import utils.TestUtils;
 
 /**
@@ -162,12 +165,12 @@ public class TestMoneroDaemon {
   
   // ------------------------------- PRIVATE ---------------------------------
   
-  class MoneroBlockTestConfig {
+  public static class MoneroBlockTestConfig {
     boolean hasTxs;
     boolean hasHex;
     boolean headerIsFull;
-    MoneroTxTestConfig txConfig;
-    public MoneroBlockTestConfig(boolean hasTxs, boolean hasHex, boolean headerIsFull, MoneroTxTestConfig txConfig) {
+    MoneroTxCtx txConfig;
+    public MoneroBlockTestConfig(boolean hasTxs, boolean hasHex, boolean headerIsFull, MoneroTxCtx txConfig) {
       super();
       this.hasTxs = hasTxs;
       this.hasHex = hasHex;
@@ -176,13 +179,16 @@ public class TestMoneroDaemon {
     }
   }
   
-  class MoneroTxTestConfig {
+  public static class MoneroTxCtx {
     boolean hasJson;
     boolean isPruned;
     boolean isFull;
     boolean isConfirmed;
     boolean isCoinbase;
     boolean fromGetTxPool;
+    boolean fromGetBlocksByHeight;
+    boolean hasOutputIndices;
+    boolean doNotTestCopy;
   }
   
   private static void testBlockTemplate(MoneroBlockTemplate template) {
@@ -198,10 +204,10 @@ public class TestMoneroDaemon {
   
   private static void testBlockHeader(MoneroBlockHeader header, boolean isFull) {
     assertNotNull(header);
-    assert(header.getHeight() >= 0);
-    assert(header.getMajorVersion() >= 0);
-    assert(header.getMinorVersion() >= 0);
-    assert(header.getTimestamp() >= 0);
+    assertTrue(header.getHeight() >= 0);
+    assertTrue(header.getMajorVersion() >= 0);
+    assertTrue(header.getMinorVersion() >= 0);
+    assertTrue(header.getTimestamp() >= 0);
     assertNotNull(header.getPrevId());
     assertNotNull(header.getNonce());
     assertNull(header.getPowHash());  // never seen defined
@@ -239,7 +245,7 @@ public class TestMoneroDaemon {
     
     if (config.hasHex) {
       assertNotNull(block.getHex());
-      assert(block.getHex().length() > 1);
+      assertTrue(block.getHex().length() > 1);
     } else {
       assertNull(block.getHex());
     }
@@ -265,7 +271,7 @@ public class TestMoneroDaemon {
     assertTrue(coinbaseTx.getUnlockTime() >= 0);
 
     // TODO: coinbase tx does not have ids in binary requests so this will fail, need to derive using prunable data
-    MoneroTxTestConfig config = new MoneroTxTestConfig();
+    MoneroTxCtx config = new MoneroTxCtx();
     config.hasJson = false;
     config.isPruned = true;
     config.isFull = false;
@@ -273,5 +279,173 @@ public class TestMoneroDaemon {
     config.isCoinbase = true;
     config.fromGetTxPool = true;
     testTx(coinbaseTx, config);
+  }
+  
+  private static void testTx(MoneroTx tx, MoneroTxCtx ctx) {
+    
+    // check inputs
+    assertNotNull(tx);
+    assertNotNull(ctx);
+    assertNotNull(ctx.isPruned);
+    assertNotNull(ctx.isConfirmed);
+    assertNotNull(ctx.fromGetTxPool);
+    
+    // standard across all txs
+    assertEquals(64, tx.getId().length());
+    if (tx.getIsRelayed() == null) assertTrue(tx.getInTxPool());  // TODO monero-daemon-rpc: add relayed to get_transactions
+    else assertNotNull(tx.getIsRelayed());
+    assertNull(tx.getSignatures());
+    assertNotNull(tx.getIsConfirmed());
+    assertNotNull(tx.getInTxPool());
+    assertNotNull(tx.getIsCoinbase());
+    assertNotNull(tx.getIsDoubleSpend());
+    assertNull(tx.getPrunableHex());
+    assertTrue(tx.getVersion() >= 0);
+    assertTrue(tx.getUnlockTime() >= 0);
+    assertTrue(tx.getVins().size() >= 0);
+    assertTrue(tx.getVouts().size() >= 0);
+    for (MoneroOutput vin : tx.getVins()) assertTrue(tx == vin.getTx());
+    for (MoneroOutput vout : tx.getVouts()) assertTrue(tx == vout.getTx());
+    assertTrue(tx.getExtra().length > 0);
+    assertNotNull(tx.getRctSignatures()); // TODO: model and return 
+    if (ctx.fromGetBlocksByHeight) assertEquals(tx.getHex(), null);  // TODO: getBlocksByHeight() has inconsistent client-side pruning
+    else assertFalse(tx.getHex().isEmpty());
+    
+    // test presence of output indices
+    // TODO: change this over to vouts only
+    if (tx.getIsCoinbase()) assertEquals(tx.getOutputIndices(), null); // TODO: how to get output indices for coinbase transactions?
+    if (tx.getInTxPool() || ctx.fromGetTxPool || ctx.hasOutputIndices == false) assertEquals(null, tx.getOutputIndices());
+    else assertFalse(tx.getOutputIndices().isEmpty());
+    
+    // test confirmed ctx
+    if (ctx.isConfirmed == true) assertEquals(true, tx.getIsConfirmed());
+    if (ctx.isConfirmed == false) assertEquals(false, tx.getIsConfirmed());
+    
+    // test confirmed
+    if (tx.getIsConfirmed()) {
+      assertNotNull(tx.getBlock());
+      assertTrue(tx.getBlock().getTxs().contains(tx));
+      assertTrue(tx.getBlock().getHeader().getHeight() > 0);
+      assertTrue(tx.getBlock().getTxs().contains(tx));
+      assertTrue(tx.getBlock().getHeader().getHeight() > 0);
+      assertTrue(tx.getBlock().getHeader().getTimestamp() > 0);
+      assertEquals(true, tx.getIsRelayed());
+      assertEquals(false, tx.getIsFailed());
+      assertEquals(false, tx.getInTxPool());
+      assertEquals(false, tx.getDoNotRelay());
+      assertEquals(false, tx.getIsDoubleSpend());
+      assertEquals(null, tx.getNumConfirmations()); // client must compute
+    } else {
+      assertEquals(null, tx.getBlock());
+      assertEquals(0, (int) tx.getNumConfirmations());
+    }
+    
+    // test in tx pool
+    if (tx.getInTxPool()) {
+      assertEquals(tx.getIsConfirmed(), false);
+      assertEquals(tx.getIsDoubleSpend(), false);
+      assertEquals(tx.getLastFailedHeight(), null);
+      assertEquals(tx.getLastFailedId(), null);
+      assertTrue(tx.getReceivedTimestamp() > 0);
+      assertTrue(tx.getNumEstimatedBlocksUntilConfirmed() > 0);
+    } else {
+      assertEquals(tx.getNumEstimatedBlocksUntilConfirmed(), null);
+      assertEquals(tx.getLastRelayedTimestamp(), null);
+    }
+    
+    // test coinbase tx
+    if (tx.getIsCoinbase()) {
+      assertEquals(0, tx.getFee().equals(BigInteger.valueOf(0)));
+      assertEquals(null, tx.getVins());
+    }
+    
+    // test failed  // TODO: what else to test associated with failed
+    if (tx.getIsFailed()) {
+      assertTrue(tx.getReceivedTimestamp() > 0);
+    } else {
+      if (tx.getIsRelayed() == null) assertEquals(null, tx.getDoNotRelay()); // TODO monero-daemon-rpc: add relayed to get_transactions
+      else if (tx.getIsRelayed()) assertEquals(false, tx.getIsDoubleSpend());
+      else {
+        assertEquals(false, tx.getIsRelayed());
+        assertEquals(true, tx.getDoNotRelay());
+        assertNotNull(tx.getIsDoubleSpend());
+      }
+    }
+    assertEquals(tx.getLastFailedHeight(), null);
+    assertEquals(tx.getLastFailedId(), null);
+    
+    // received time only for tx pool or failed txs
+    if (tx.getReceivedTimestamp() != null) {
+      assertTrue(tx.getInTxPool() || tx.getIsFailed());
+    }
+    
+    // test relayed tx
+    if (tx.getIsRelayed()) assertEquals(tx.getDoNotRelay(), false);
+    if (tx.getDoNotRelay()) {
+      assertTrue(!tx.getIsRelayed());
+      assertTrue(!tx.getIsConfirmed());
+    }
+    
+    // test vins and vouts
+    if (!tx.getIsCoinbase()) assertFalse(tx.getVins().isEmpty());
+    assertFalse(tx.getVouts().isEmpty());
+    if (tx.getVins() != null) for (MoneroOutput vin : tx.getVins()) testVin(vin, ctx);
+    if (tx.getVouts() != null) for (MoneroOutput vout : tx.getVouts()) testVout(vout, ctx);
+    
+    // test pruned vs not pruned
+    if (ctx.isPruned) {
+      assertEquals(null, tx.getRctSigPrunable());
+      assertEquals(null, tx.getSize());
+      assertEquals(null, tx.getLastRelayedTimestamp());
+      assertEquals(null, tx.getReceivedTimestamp());
+      assertEquals(null, tx.getPrunedHex());
+      assertFalse(tx.getPrunedHex().isEmpty());
+    } else {
+      if (ctx.fromGetBlocksByHeight) assertEquals(null, tx.getRctSigPrunable());  // TODO: getBlocksByHeight() has inconsistent client-side pruning
+      else assertNotNull(tx.getRctSigPrunable());
+      assertEquals(null, tx.getPrunedHex());
+      assertEquals(false, tx.getIsDoubleSpend());
+      if (tx.getIsConfirmed()) {
+        assertEquals(null, tx.getLastRelayedTimestamp());
+        assertEquals(null, tx.getReceivedTimestamp());
+      } else {
+        if (tx.getIsRelayed()) assertTrue(tx.getLastRelayedTimestamp() > 0);
+        else assertEquals(null, tx.getLastRelayedTimestamp());
+        assertTrue(tx.getReceivedTimestamp() > 0);
+      }
+      assertEquals(null, tx.getPrunableHash());
+      
+//      if (ctx.fromGetTxPool || ctx.fromGetBlocksByHeight) assertEquals(tx.getPrunableHash(), null);  // TODO: getBlocksByHeight() has inconsistent client-side pruning
+//      else assertFalse(tx.getPrunableHash().isEmpty());
+//      
+//      if (ctx.isPruned) assertNull(tx.getPrunableHash()); // TODO: tx may or may not have prunable hash, need to know when it's expected
+//      else assertFalse(tx.getPrunableHash().isEmpty());
+    }
+    
+    // test fields from tx pool
+    if (ctx.fromGetTxPool) {
+      assertTrue(tx.getSize() > 0);
+      assertTrue(tx.getWeight() > 0);
+      assertNotNull(tx.getIsKeptByBlock());
+      assertEquals(null, tx.getLastFailedHeight());
+      assertEquals(null, tx.getLastFailedId());
+      assertTrue(tx.getMaxUsedBlockHeight() >= 0);
+      assertTrue(tx.getMaxUsedBlockId() > 0);
+    } else {
+      assertEquals(null, tx.getWeight());
+      assertEquals(null, tx.getIsKeptByBlock());
+      assertEquals(null ,tx.getIsFailed());
+      assertEquals(null, tx.getLastFailedHeight());
+      assertEquals(null, tx.getLastFailedId());
+      assertEquals(null, tx.getMaxUsedBlockHeight());
+      assertEquals(null, tx.getMaxUsedBlockId());
+    }
+    
+    if (tx.getIsFailed()) {
+      // TODO: implement this
+    }
+    
+    // test deep copy
+    if (!ctx.doNotTestCopy) testTxCopy(tx, ctx);
   }
 }
