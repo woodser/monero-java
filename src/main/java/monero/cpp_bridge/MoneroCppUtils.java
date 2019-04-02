8f8af774061ddae3e01d92b59f75542d00e79c84
woodser <woodser@protonmail.com>
2019-04-02 11:27:50 -0400

    getBlocksByHeight(), MoneroCppUtils placeholders

---
+++ src/main/java/monero/cpp_bridge/MoneroCppUtils.java
@@ -0,0 +1,17 @@
+package monero.cpp_bridge;
+
+import java.util.Map;
+
+/**
+ * Collection of utilties bridged from Monero Core C++ to Java.
+ */
+public class MoneroCppUtils {
+
+  public static Map<String, Object> binaryBlocksToMap(int[] binBlocks) {
+    throw new RuntimeException("Not implemented");
+  }
+  
+  public static int[] mapToBinary(Map<String, Object> map) {
+    throw new RuntimeException("Not implemented");
+  }
+}
