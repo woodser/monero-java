23e4dce3fb3cace3a68eaa7a9d1cfd43a802b7cb
woodser <woodser@protonmail.com>
2019-04-24 14:19:54 -0400

    refactored MoneroTransferFilter.meetsCriteria() with new transfer model

--- src/main/java/monero/wallet/config/MoneroTransferFilter.java
+++ src/main/java/monero/wallet/config/MoneroTransferFilter.java
@@ -1,10 +1,13 @@
 package monero.wallet.config;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
 
 import common.types.Filter;
 import monero.wallet.model.MoneroDestination;
+import monero.wallet.model.MoneroIncomingTransfer;
+import monero.wallet.model.MoneroOutgoingTransfer;
 import monero.wallet.model.MoneroTransfer;
 import monero.wallet.model.MoneroTxWallet;
 
@@ -105,32 +108,58 @@ public class MoneroTransferFilter extends MoneroTransfer implements Filter<Moner
 
   @Override
   public boolean meetsCriteria(MoneroTransfer transfer) {
-//    if (transfer == null) return false;
-//    
-//    // filter on transfer fields
-//    if (this.getAddress() != null && !this.getAddress().equals(transfer.getAddress())) return false;
-//    if (this.getAccountIndex() != null && !this.getAccountIndex().equals(transfer.getAccountIndex())) return false;
-//    if (this.getSubaddressIndex() != null && !this.getSubaddressIndex().equals(transfer.getSubaddressIndex())) return false;
-//    if (this.getAmount() != null && this.getAmount().compareTo(transfer.getAmount()) != 0) return false;
-//    
-//    // filter extensions
-//    if (this.getIsIncoming() != null && this.getIsIncoming() != transfer.getIsIncoming()) return false;
-//    if (this.getIsOutgoing() != null && this.getIsOutgoing() != transfer.getIsOutgoing()) return false;
-//    if (this.getSubaddressIndices() != null && !this.getSubaddressIndices().contains(transfer.getSubaddressIndex())) return false;
-//    if (this.getHasDestinations() != null) {
-//      if (this.getHasDestinations() && transfer.getDestinations() == null) return false;
-//      if (!this.getHasDestinations() && transfer.getDestinations() != null) return false;
-//    }
-//    
-//    // filter with transaction filter
-//    if (this.getTxFilter() != null && !this.getTxFilter().meetsCriteria(transfer.getTx())) return false;
-//    
-//    // filter on destinations TODO: start with test for this
-////  if (this.getDestionations() != null && this.getDestionations() != transfer.getDestionations()) return false;
-//    
-//    // transfer meets filter criteria
-//    return true;
-    throw new RuntimeException("Update code above");
+    if (transfer == null) return false;
+    
+    // filter on common fields
+    if (this.getIsIncoming() != null && this.getIsIncoming() != transfer.getIsIncoming()) return false;
+    if (this.getIsOutgoing() != null && this.getIsOutgoing() != transfer.getIsOutgoing()) return false;
+    if (this.getAmount() != null && this.getAmount().compareTo(transfer.getAmount()) != 0) return false;
+    if (this.getAccountIndex() != null && !this.getAccountIndex().equals(transfer.getAccountIndex())) return false;
+    
+    // filter on incoming fields
+    if (transfer instanceof MoneroIncomingTransfer) {
+      MoneroIncomingTransfer inTransfer = (MoneroIncomingTransfer) transfer;
+      if (this.getAddress() != null && !this.getAddress().equals(inTransfer.getAddress())) return false;
+      if (this.getAddresses() != null && !this.getAddresses().contains(inTransfer.getAddress())) return false;
+      if (this.getSubaddressIndex() != null && !this.getSubaddressIndex().equals(inTransfer.getSubaddressIndex())) return false;
+      if (this.getSubaddressIndices() != null && !this.getSubaddressIndices().contains(inTransfer.getSubaddressIndex())) return false;
+    }
+
+    // filter on outgoing fields
+    else if (transfer instanceof MoneroOutgoingTransfer) {
+      MoneroOutgoingTransfer outTransfer = (MoneroOutgoingTransfer) transfer;
+      
+      // filter on addresses
+      if (this.getAddress() != null && (outTransfer.getAddresses() == null || !outTransfer.getAddresses().contains(this.getAddress()))) return false;   // TODO: will filter all transfers if they don't contain addresses
+      if (this.getAddresses() != null) {
+        List<String> intersections = new ArrayList<String>(this.getAddresses());
+        intersections.retainAll(outTransfer.getAddresses());
+        if (intersections.isEmpty()) return false;  // must have overlapping addresses
+      }
+      
+      // filter on subaddress indices
+      if (this.getSubaddressIndex() != null && (outTransfer.getSubaddressIndices() == null || !outTransfer.getSubaddressIndices().contains(this.getSubaddressIndex()))) return false;
+      if (this.getSubaddressIndices() != null) {
+        List<Integer> intersections = new ArrayList<Integer>(this.getSubaddressIndices());
+        intersections.retainAll(outTransfer.getSubaddressIndices());
+        if (intersections.isEmpty()) return false;  // must have overlapping subaddress indices
+      }
+      
+      // filter on having destinations
+      if (this.getHasDestinations() != null) {
+        if (this.getHasDestinations() && outTransfer.getDestinations() == null) return false;
+        if (!this.getHasDestinations() && outTransfer.getDestinations() != null) return false;
+      }
+      
+      // filter on destinations TODO: start with test for this
+//    if (this.getDestionations() != null && this.getDestionations() != transfer.getDestionations()) return false;
+    } else throw new RuntimeException("Transfer must be MoneroIncomingTransfer or MoneroOutgoingTransfer");
+    
+    // filter with tx filter
+    if (this.getTxFilter() != null && !this.getTxFilter().meetsCriteria(transfer.getTx())) return false;
+    
+    // transfer meets filter criteria
+    return true;
   }
   
   // ------------------- OVERRIDE CO-VARIANT RETURN TYPES ---------------------
