80370daa3b0e487052a35b22a47f9e6bf823f30c
woodser <woodser@protonmail.com>
2019-04-28 08:01:59 -0400

    StopMining.java calls daemon instead of wallet

--- src/test/java/utils/StopMining.java
+++ src/test/java/utils/StopMining.java
@@ -3,6 +3,6 @@ package utils;
 public class StopMining {
 
   public static void main(String[] args) {
-    TestUtils.getWalletRpc().stopMining();
+    TestUtils.getDaemonRpc().stopMining();
   }
 }
