2e19b5c2bcb1e4b2619911d211276e57412fdfc6
woodser <woodser@protonmail.com>
2019-05-06 13:01:18 -0400

    mine to different wallet
    Former-commit-id: 7fb81e1046f824507de66a3a46d521ea57ef9229

--- src/test/java/utils/StartMining.java
+++ src/test/java/utils/StartMining.java
@@ -3,6 +3,8 @@ package utils;
 public class StartMining {
 
   public static void main(String[] args) {
-    TestUtils.getWalletRpc().startMining(7, false, true);
+    int numThreads = 8;
+    //TestUtils.getWalletRpc().startMining(numThreads, false, true);
+    TestUtils.getDaemonRpc().startMining("59HdmrdyhUEMyuzhLGLVTjPEm4kHaHQc5gGVXRBjxaXDEU4M6G2mRv7KZipYyK2QS9G7FLCg4bGc2Wqv5H86zjEMF1UhbzZ", numThreads, false, false);
   }
 }
