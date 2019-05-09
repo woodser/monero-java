da37ee201c19327d42cceac3a5c03721dde1e9b9
woodser <woodser@protonmail.com>
2019-05-09 13:09:14 -0400

    temporarily disable wallet jni code

--- src/main/cpp/monero_wallet_MoneroWalletJni.cpp
+++ src/main/cpp/monero_wallet_MoneroWalletJni.cpp
@@ -5,23 +5,26 @@ using namespace std;
 
 JNIEXPORT jlong JNICALL
 Java_monero_wallet_MoneroWalletJni_openWalletJni(JNIEnv *env, jclass clazz, jstring path, jstring password, jint networkType) {
-    const char* _path = env->GetStringUTFChars(path, NULL);
-    const char* _password = env->GetStringUTFChars(password, NULL);
-    Monero::NetworkType _networkType = static_cast<Monero::NetworkType>(networkType);
-
-    Bitmonero::Wallet *wallet =
-            Bitmonero::WalletManagerFactory::getWalletManager()->openWallet(
-                    std::string(_path),
-                    std::string(_password),
-                    _networkType);
-
-    env->ReleaseStringUTFChars(path, _path);
-    env->ReleaseStringUTFChars(password, _password);
-    return reinterpret_cast<jlong>(wallet);
+//    const char* _path = env->GetStringUTFChars(path, NULL);
+//    const char* _password = env->GetStringUTFChars(password, NULL);
+//    Monero::NetworkType _networkType = static_cast<Monero::NetworkType>(networkType);
+//
+//    Bitmonero::Wallet *wallet =
+//            Bitmonero::WalletManagerFactory::getWalletManager()->openWallet(
+//                    std::string(_path),
+//                    std::string(_password),
+//                    _networkType);
+//
+//    env->ReleaseStringUTFChars(path, _path);
+//    env->ReleaseStringUTFChars(password, _password);
+//    string str = "";
+//    return reinterpret_cast<jlong>(str);
 }
 
-JNIEXPORT jlong JNICALL
+JNIEXPORT jstring JNICALL
 Java_monero_wallet_MoneroWalletJni_getBalanceWalletJni(JNIEnv *env, jobject instance) {
-    Bitmonero::Wallet *wallet = getHandle<Bitmonero::Wallet>(env, instance);
-    return wallet->balanceAll();
+    //Bitmonero::Wallet *wallet = getHandle<Bitmonero::Wallet>(env, instance);
+//    //return wallet->balanceAll();
+//    string str = "";
+//    return env->NewStringUTF(str.c_str());
 }
