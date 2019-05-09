76e0ebed5c0103ac4953488e2be955a64ba744f0
woodser <woodser@protonmail.com>
2019-05-09 10:39:23 -0400

    replace MoneroWalletLocal with MoneroWalletJni
    Former-commit-id: 53ef2caefd2d0adf59a9cfbfbdb9899c5ea1108f

---
+++ src/test/java/test/TestMoneroWalletJni.java
@@ -0,0 +1,455 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.BeforeClass;
+
+import monero.daemon.MoneroDaemon;
+import monero.wallet.MoneroWallet;
+import monero.wallet.MoneroWalletJni;
+import monero.wallet.model.MoneroIncomingTransfer;
+import monero.wallet.model.MoneroOutgoingTransfer;
+import monero.wallet.model.MoneroTransfer;
+
+/**
+ * Tests the JNI wallet specifically.
+ */
+public class TestMoneroWalletJni extends TestMoneroWalletCommon {
+  
+  protected MoneroWalletJni wallet;
+  
+  public TestMoneroWalletJni() {
+    this.wallet = (MoneroWalletJni) getTestWallet();
+  }
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+    
+  }
+
+  @Override
+  protected MoneroWallet getTestWallet() {
+    return new MoneroWalletJni("test_wallet_1");
+  }
+  
+  // -------------------- OVERRIDES TO BE DIRECTLY RUNNABLE -------------------
+
+  @Override
+  public void testGetHeight() {
+    super.testGetHeight();
+  }
+
+  @Override
+  public void testGetMnemonic() {
+    super.testGetMnemonic();
+  }
+
+  @Override
+  public void testGetSupportedLanguages() {
+    super.testGetSupportedLanguages();
+  }
+
+  @Override
+  public void testGetPrivateViewKey() {
+    super.testGetPrivateViewKey();
+  }
+
+  @Override
+  public void testGetPrimaryAddress() {
+    super.testGetPrimaryAddress();
+  }
+
+  @Override
+  public void testGetIntegratedAddressFromPaymentId() {
+    super.testGetIntegratedAddressFromPaymentId();
+  }
+
+  @Override
+  public void testDecodeIntegratedAddress() {
+    super.testDecodeIntegratedAddress();
+  }
+
+  @Override
+  public void testSyncWithoutProgress() {
+    super.testSyncWithoutProgress();
+  }
+
+  @Override
+  public void testGetAccountsWithoutSubaddresses() {
+    super.testGetAccountsWithoutSubaddresses();
+  }
+
+  @Override
+  public void testGetAccountsWithSubaddresses() {
+    super.testGetAccountsWithSubaddresses();
+  }
+
+  @Override
+  public void testGetAccount() {
+    super.testGetAccount();
+  }
+
+  @Override
+  public void testCreateAccountWithoutLabel() {
+    super.testCreateAccountWithoutLabel();
+  }
+
+  @Override
+  public void testCreateAccountWithLabel() {
+    super.testCreateAccountWithLabel();
+  }
+
+  @Override
+  public void testGetSubaddresses() {
+    super.testGetSubaddresses();
+  }
+
+  @Override
+  public void testGetSubaddressesByIndices() {
+    super.testGetSubaddressesByIndices();
+  }
+
+  @Override
+  public void testGetSubaddressByIndex() {
+    super.testGetSubaddressByIndex();
+  }
+
+  @Override
+  public void testCreateSubaddress() {
+    super.testCreateSubaddress();
+  }
+
+  @Override
+  public void testGetSubaddressAddress() {
+    super.testGetSubaddressAddress();
+  }
+
+  @Override
+  public void testGetAddressIndices() {
+    super.testGetAddressIndices();
+  }
+
+  @Override
+  public void getAllBalances() {
+    super.getAllBalances();
+  }
+
+  @Override
+  public void testGetTxsWallet() {
+    super.testGetTxsWallet();
+  }
+  
+  @Override
+  public void testGetTxsById() {
+    super.testGetTxsById();
+  }
+
+  @Override
+  public void testGetTxsWithConfiguration() {
+    super.testGetTxsWithConfiguration();
+  }
+
+  @Override
+  public void testGetTxsWithPaymentIds() {
+    super.testGetTxsWithPaymentIds();
+  }
+
+  @Override
+  public void testGetTxsFieldsWithFiltering() {
+    super.testGetTxsFieldsWithFiltering();
+  }
+
+  @Override
+  public void testGetTxsValidateInputs() {
+    super.testGetTxsValidateInputs();
+  }
+
+  @Override
+  public void testGetTransfers() {
+    super.testGetTransfers();
+  }
+
+  @Override
+  public void testGetTransfersWithConfiguration() {
+    super.testGetTransfersWithConfiguration();
+  }
+
+  @Override
+  public void testGetTransfersValidateInputs() {
+    super.testGetTransfersValidateInputs();
+  }
+
+  @Override
+  public void testGetOutputs() {
+    super.testGetOutputs();
+  }
+
+  @Override
+  public void testGetOutputsWithConfiguration() {
+    super.testGetOutputsWithConfiguration();
+  }
+
+  @Override
+  public void testGetOutputsValidateInputs() {
+    super.testGetOutputsValidateInputs();
+  }
+
+  @Override
+  public void testAccounting() {
+    super.testAccounting();
+  }
+
+  @Override
+  public void testSetTransactionNote() {
+    super.testSetTransactionNote();
+  }
+
+  @Override
+  public void testSetTransactionNotes() {
+    super.testSetTransactionNotes();
+  }
+
+  @Override
+  public void testCheckTxKey() {
+    super.testCheckTxKey();
+  }
+
+  @Override
+  public void testCheckTxProof() {
+    super.testCheckTxProof();
+  }
+
+  @Override
+  public void testCheckSpendProof() {
+    super.testCheckSpendProof();
+  }
+
+  @Override
+  public void testGetReserveProofWallet() {
+    super.testGetReserveProofWallet();
+  }
+
+  @Override
+  public void testGetReserveProofAccount() {
+    super.testGetReserveProofAccount();
+  }
+
+  @Override
+  public void testGetOutputsHex() {
+    super.testGetOutputsHex();
+  }
+
+  @Override
+  public void testImportOutputsHex() {
+    super.testImportOutputsHex();
+  }
+
+  @Override
+  public void testGetSignedKeyImages() {
+    super.testGetSignedKeyImages();
+  }
+
+  @Override
+  public void testGetNewKeyImagesFromLastImport() {
+    super.testGetNewKeyImagesFromLastImport();
+  }
+
+  @Override
+  public void testImportKeyImages() {
+    super.testImportKeyImages();
+  }
+
+  @Override
+  public void testSignAndVerifyMessages() {
+    super.testSignAndVerifyMessages();
+  }
+
+  @Override
+  public void testSetKeyValues() {
+    super.testSetKeyValues();
+  }
+
+  @Override
+  public void testCreatePaymentUri() {
+    super.testCreatePaymentUri();
+  }
+
+  @Override
+  public void testMining() {
+    super.testMining();
+  }
+
+  @Override
+  public void testSendFromSubaddresses() {
+    super.testSendFromSubaddresses();
+  }
+
+  @Override
+  public void testSendFromSubaddressesSplit() {
+    super.testSendFromSubaddressesSplit();
+  }
+
+  @Override
+  public void testSend() {
+    super.testSend();
+  }
+
+  @Override
+  public void testSendWithPaymentId() {
+    super.testSendWithPaymentId();
+  }
+
+  @Override
+  public void testSendWithRingSize() {
+    super.testSendWithRingSize();
+  }
+
+  @Override
+  public void testSendSplit() {
+    super.testSendSplit();
+  }
+
+  @Override
+  public void testCreateThenRelay() {
+    super.testCreateThenRelay();
+  }
+
+  @Override
+  public void testCreateThenRelaySplit() {
+    super.testCreateThenRelaySplit();
+  }
+
+  @Override
+  public void testSendToMultiple() {
+    super.testSendToMultiple();
+  }
+
+  @Override
+  public void testSendToMultipleSplit() {
+    super.testSendToMultipleSplit();
+  }
+  
+  @Override
+  public void testSendDustToMultipleSplit() {
+    super.testSendDustToMultipleSplit();
+  }
+
+  @Override
+  public void testUpdateLockedSameAccount() {
+    super.testUpdateLockedSameAccount();
+  }
+
+  @Override
+  public void testUpdateLockedSameAccountSplit() {
+    super.testUpdateLockedSameAccountSplit();
+  }
+
+  @Override
+  public void testUpdateLockedDifferentAccounts() {
+    super.testUpdateLockedDifferentAccounts();
+  }
+
+  @Override
+  public void testUpdateLockedDifferentAccountsSplit() {
+    super.testUpdateLockedDifferentAccountsSplit();
+  }
+  
+  @Override
+  public void testSweepOutputs() {
+    super.testSweepOutputs();
+  }
+
+  @Override
+  public void testSweepSubaddresses() {
+    super.testSweepSubaddresses();
+  }
+
+  @Override
+  public void testSweepAccounts() {
+    super.testSweepAccounts();
+  }
+
+  @Override
+  public void testSweepWalletByAccounts() {
+    super.testSweepWalletByAccounts();
+  }
+  
+  @Override
+  public void testSweepWalletBySubaddresses() {
+    super.testSweepWalletBySubaddresses();
+  }
+
+  @Override
+  public void testSweepDustNoRelay() {
+    super.testSweepDustNoRelay();
+  }
+
+  @Override
+  public void testSweepDust() {
+    super.testSweepDust();
+  }
+  
+  @Override
+  public void testRescanBlockchain() {
+    super.testRescanBlockchain();
+  }
+
+  @Override
+  protected MoneroDaemon getTestDaemon() {
+    return super.getTestDaemon();
+  }
+  
+  @SuppressWarnings("unchecked")
+  private static void compareTransferOrder(List<Map<String, Object>> rpcTransfers, List<?> transfers) {
+    if (rpcTransfers == null) {
+      assertTrue(transfers.isEmpty());
+      return;
+    }
+    assertEquals(rpcTransfers.size(), transfers.size());
+    for (int i = 0; i < transfers.size(); i++) {
+      MoneroTransfer transfer = (MoneroTransfer) transfers.get(i);
+      Map<String, Object> rpcTransfer = rpcTransfers.get(i);
+      assertEquals((String) rpcTransfer.get("txid"), transfer.getTx().getId());
+      
+      // collect account and subaddress indices from rpc response
+      List<Map<String, BigInteger>> rpcSubaddrIndices = (List<Map<String, BigInteger>>) rpcTransfer.get("subaddr_indices");
+      Integer accountIdx = null;
+      List<Integer> subaddressIndices = new ArrayList<Integer>();
+      for (Map<String, BigInteger> rpcSubaddrIdx : rpcSubaddrIndices) {
+        if (accountIdx == null) accountIdx = rpcSubaddrIdx.get("major").intValue();
+        else assertEquals((int) accountIdx, (int) rpcSubaddrIdx.get("major").intValue());
+        subaddressIndices.add(rpcSubaddrIdx.get("minor").intValue());
+      }
+      
+      // test transfer
+      assertEquals(accountIdx, transfer.getAccountIndex());
+      if (transfer instanceof MoneroIncomingTransfer) {
+        assertEquals(1, rpcSubaddrIndices.size());
+        assertEquals(subaddressIndices.get(0), ((MoneroIncomingTransfer) transfer).getSubaddressIndex());
+      } else if (transfer instanceof MoneroOutgoingTransfer) {
+        assertEquals(subaddressIndices, ((MoneroOutgoingTransfer) transfer).getSubaddressIndices());
+      } else {
+        fail("Unrecognized transfer instance");
+      }
+    }
+  }
+  
+//  private static void compareTxOrder(List<Map<String, Object>> rpcTransfers, List<MoneroTxWallet> txs) {
+//    List<String> txIds = new ArrayList<String>();
+//    for (Map<String, Object> rpcTransfer : rpcTransfers) {
+//      String txId = (String) rpcTransfer.get("txid");
+//      if (!txIds.contains(txId)) txIds.add(txId);
+//    }
+//    assertEquals(txIds.size(), txs.size());
+//    for (int i = 0; i < txIds.size(); i++) {
+//      assertEquals(txIds.get(i), txs.get(i).getId());
+//    }
+//  }
+}
