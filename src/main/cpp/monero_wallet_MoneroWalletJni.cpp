c28eed8653f8b438d69786b7e0ea753645379eef
woodser <woodser@protonmail.com>
2019-05-16 08:13:33 -0400

    wip openWalletJni()
    Former-commit-id: 350cd4feeceb0031a20c4c1e8ad5fa4d2581a2c4

--- src/main/cpp/monero_wallet_MoneroWalletJni.cpp
+++ src/main/cpp/monero_wallet_MoneroWalletJni.cpp
@@ -210,37 +210,59 @@ Java_monero_wallet_MoneroWalletJni_openWalletJni(JNIEnv *env, jclass clazz, jstr
 
 //  new tools::wallet2(static_cast<cryptonote::network_type>(nettype), kdf_rounds, true)]
 
-  cout << "Starting creation of vm" << endl;
+//  cout << "Starting creation of vm" << endl;
+//
+//  namespace po = boost::program_options;
+//  po::options_description desc("dummy");
+//  const command_line::arg_descriptor<std::string, true> arg_password = {"password", "Specifies the password to decrypt the wallet"};
+//  const char *argv[4];
+//  int argc = 3;
+//  argv[0] = "wallet-rpc";
+//  argv[1] = "--password";
+//  argv[2] = std::string(_password).c_str();
+//  argv[3] = NULL;
+//  po::variables_map vm;
+//  command_line::add_arg(desc, arg_password);
+//  po::store(po::parse_command_line(argc, argv, desc), vm);
+//
+//  //const command_line::arg_descriptor<std::string, true> arg_password = {"password", "Specifies the password to decrypt the wallet"};
+//  const auto vmPassword = command_line::get_arg(vm, arg_password);
+//  cout << "Stored password: " << vmPassword << endl;
+//
+//  cout << "Calling make_from_file" << endl;
+//  cout << "Path: " + std::string(_path) << endl;
+//
+//  std::unique_ptr<tools::wallet2> wallet = tools::wallet2::make_from_file(vm, true, std::string(_path), nullptr).first;
+//
+//  cout << "Ready to return" << endl;
+//
+//  cout << wallet << endl;
+//
+//  env->ReleaseStringUTFChars(path, _path);
+//  env->ReleaseStringUTFChars(password, _password);
+//  //return reinterpret_cast<jlong>(wallet);
 
-  namespace po = boost::program_options;
-  po::options_description desc("dummy");
-  const command_line::arg_descriptor<std::string, true> arg_password = {"password", "Specifies the password to decrypt the wallet"};
-  const char *argv[4];
-  int argc = 3;
-  argv[0] = "wallet-rpc";
-  argv[1] = "--password";
-  argv[2] = std::string(_password).c_str();
-  argv[3] = NULL;
-  po::variables_map vm;
-  command_line::add_arg(desc, arg_password);
-  po::store(po::parse_command_line(argc, argv, desc), vm);
+//  cout << "Defining... " << endl;
+//
+//  //std::unique_ptr<tools::wallet2> wallet = tools::wallet2::make_from_file(nullptr, true, std::string(_path), nullptr).first;
+//
+//
+  //tools::wallet2* wallet = new tools::wallet2(static_cast<cryptonote::network_type>(1), 0, true);
 
-  //const command_line::arg_descriptor<std::string, true> arg_password = {"password", "Specifies the password to decrypt the wallet"};
-  const auto vmPassword = command_line::get_arg(vm, arg_password);
-  cout << "Stored password: " << vmPassword << endl;
+  //tools::wallet2 m_wallet;
+  //m_wallet.reset(new tools::wallet2(static_cast<cryptonote::network_type>(-), 1, true))
+  std::unique_ptr<tools::wallet2> wallet(new tools::wallet2(static_cast<cryptonote::network_type>(2), 1, true));
 
-  cout << "Calling make_from_file" << endl;
-  cout << "Path: " + std::string(_path) << endl;
 
-  std::unique_ptr<tools::wallet2> wallet = tools::wallet2::make_from_file(vm, true, std::string(_path), nullptr).first;
-
-  cout << "Ready to return" << endl;
-
-  cout << wallet << endl;
-
-  env->ReleaseStringUTFChars(path, _path);
-  env->ReleaseStringUTFChars(password, _password);
-  //return reinterpret_cast<jlong>(wallet);
+//
+//
+//  cout << "Loading... " << endl;
+//
+  //wallet->load(std::string(_path), std::string(_password));
+//
+//
+//
+  //cout << "Seed language: " + wallet->get_seed_language() << endl;
 
   throw std::runtime_error("Not implemented: Java_monero_wallet_MoneroWalletJni_openWalletJni");
 }
