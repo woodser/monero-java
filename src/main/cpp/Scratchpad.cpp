b9c5eff3691190e44f0190ddb97962c814697f9e
woodser <woodser@protonmail.com>
2019-05-17 10:28:56 -0400

    scratchpad walletExists() works with wallet2; CMakeLists.txt boost cleanup
    Former-commit-id: 075935b57693cdf441cbc471735f8da46e502c33

--- src/main/cpp/Scratchpad.cpp
+++ src/main/cpp/Scratchpad.cpp
@@ -1,17 +1,30 @@
 #include <stdio.h>
 #include <iostream>
 #include "wallet2.h"
+//#include <boost/stacktrace.hpp>
 using namespace std;
 
+bool walletExists(string path) {
+  //std::cout << boost::stacktrace::stacktrace();
+  bool keys_file_exists;
+  bool wallet_file_exists;
+  tools::wallet2::wallet_exists(path, keys_file_exists, wallet_file_exists);
+  return wallet_file_exists;
+  throw runtime_error("Not implemented");
+}
+
 /**
- * Main entry point.
+ * Scratchppad main entry point.
  */
 int main(int argc, const char* argv[]) {
 
-  cout << "Hello world!" << endl;
-
-  // print each argument from the command line
+  // print header
+  cout << "===== Scratchpad =====" << endl;
   for (int i = 0; i < argc; i++) {
-    printf("arg %d: %s\n", i, argv[i]);
+    printf("Argument %d: %s\n", i, argv[i]);
   }
+  cout << endl;
+
+  cout << "Wallet exists: " << walletExists("test_wallet_2") << endl;
 }
+
