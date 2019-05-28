d76b272a474f5cc6dcd4ef9243937b68f236338d
woodser <woodser@protonmail.com>
2019-05-28 10:57:47 -0400

    StartMining.java uses random stagenet address

--- src/test/java/utils/StartMining.java
+++ src/test/java/utils/StartMining.java
@@ -5,6 +5,6 @@ public class StartMining {
   public static void main(String[] args) {
     int numThreads = 8;
     //TestUtils.getWalletRpc().startMining(numThreads, false, true);
-    TestUtils.getDaemonRpc().startMining("59HdmrdyhUEMyuzhLGLVTjPEm4kHaHQc5gGVXRBjxaXDEU4M6G2mRv7KZipYyK2QS9G7FLCg4bGc2Wqv5H86zjEMF1UhbzZ", numThreads, false, false);
+    TestUtils.getDaemonRpc().startMining("56SWsnhejUTbgNs2EgyXdfNXUawymMMuAC9voZZSQrHzJHNxGsAvMnoUja7JcKVtPwNc1oKAkoAt1cv6EmtKRQ22U37B7cT", numThreads, false, false);  // random subaddress
   }
 }
