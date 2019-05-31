8c87808d0abebae4ff5b0299639dc4123c5abfb1
woodser <woodser@protonmail.com>
2019-05-31 11:12:10 -0400

    testGetAccountsWith/WithoutSubaddresses() passing

--- src/test/java/test/TestMoneroWalletJni.java
+++ src/test/java/test/TestMoneroWalletJni.java
@@ -313,363 +313,363 @@ public class TestMoneroWalletJni extends TestMoneroWalletCommon {
 
   // -------------------- OVERRIDES TO BE DIRECTLY RUNNABLE -------------------
 
-//  @Override
-//  public void testGetHeight() {
-//    super.testGetHeight();
-//  }
-//
-//  @Override
-//  public void testGetMnemonic() {
-//    super.testGetMnemonic();
-//  }
-//
-//  @Override
-//  public void testGetSupportedLanguages() {
-//    super.testGetSupportedLanguages();
-//  }
-//
-//  @Override
-//  public void testGetPrivateViewKey() {
-//    super.testGetPrivateViewKey();
-//  }
-//
-//  @Override
-//  public void testGetPrimaryAddress() {
-//    super.testGetPrimaryAddress();
-//  }
-//
-//  @Override
-//  public void testGetIntegratedAddressFromPaymentId() {
-//    super.testGetIntegratedAddressFromPaymentId();
-//  }
-//
-//  @Override
-//  public void testDecodeIntegratedAddress() {
-//    super.testDecodeIntegratedAddress();
-//  }
-//
-//  @Override
-//  public void testSyncWithoutProgress() {
-//    super.testSyncWithoutProgress();
-//  }
-//
-//  @Override
-//  public void testGetAccountsWithoutSubaddresses() {
-//    super.testGetAccountsWithoutSubaddresses();
-//  }
-//
-//  @Override
-//  public void testGetAccountsWithSubaddresses() {
-//    super.testGetAccountsWithSubaddresses();
-//  }
-//
-//  @Override
-//  public void testGetAccount() {
-//    super.testGetAccount();
-//  }
-//
-//  @Override
-//  public void testCreateAccountWithoutLabel() {
-//    super.testCreateAccountWithoutLabel();
-//  }
-//
-//  @Override
-//  public void testCreateAccountWithLabel() {
-//    super.testCreateAccountWithLabel();
-//  }
-//
-//  @Override
-//  public void testGetSubaddresses() {
-//    super.testGetSubaddresses();
-//  }
-//
-//  @Override
-//  public void testGetSubaddressesByIndices() {
-//    super.testGetSubaddressesByIndices();
-//  }
-//
-//  @Override
-//  public void testGetSubaddressByIndex() {
-//    super.testGetSubaddressByIndex();
-//  }
-//
-//  @Override
-//  public void testCreateSubaddress() {
-//    super.testCreateSubaddress();
-//  }
-//
-//  @Override
-//  public void testGetSubaddressAddress() {
-//    super.testGetSubaddressAddress();
-//  }
-//
-//  @Override
-//  public void testGetAddressIndices() {
-//    super.testGetAddressIndices();
-//  }
-//
-//  @Override
-//  public void testGetAllBalances() {
-//    super.testGetAllBalances();
-//  }
-//
-//  @Override
-//  public void testGetTxsWallet() {
-//    super.testGetTxsWallet();
-//  }
-//
-//  @Override
-//  public void testGetTxsById() {
-//    super.testGetTxsById();
-//  }
-//
-//  @Override
-//  public void testGetTxsWithConfiguration() {
-//    super.testGetTxsWithConfiguration();
-//  }
-//
-//  @Override
-//  public void testGetTxsWithPaymentIds() {
-//    super.testGetTxsWithPaymentIds();
-//  }
-//
-//  @Override
-//  public void testGetTxsFieldsWithFiltering() {
-//    super.testGetTxsFieldsWithFiltering();
-//  }
-//
-//  @Override
-//  public void testGetTxsValidateInputs() {
-//    super.testGetTxsValidateInputs();
-//  }
-//
-//  @Override
-//  public void testGetTransfers() {
-//    super.testGetTransfers();
-//  }
-//
-//  @Override
-//  public void testGetTransfersWithConfiguration() {
-//    super.testGetTransfersWithConfiguration();
-//  }
-//
-//  @Override
-//  public void testGetTransfersValidateInputs() {
-//    super.testGetTransfersValidateInputs();
-//  }
-//
-//  @Override
-//  public void testGetOutputs() {
-//    super.testGetOutputs();
-//  }
-//
-//  @Override
-//  public void testGetOutputsWithConfiguration() {
-//    super.testGetOutputsWithConfiguration();
-//  }
-//
-//  @Override
-//  public void testGetOutputsValidateInputs() {
-//    super.testGetOutputsValidateInputs();
-//  }
-//
-//  @Override
-//  public void testAccounting() {
-//    super.testAccounting();
-//  }
-//
-//  @Override
-//  public void testSetTransactionNote() {
-//    super.testSetTransactionNote();
-//  }
-//
-//  @Override
-//  public void testSetTransactionNotes() {
-//    super.testSetTransactionNotes();
-//  }
-//
-//  @Override
-//  public void testCheckTxKey() {
-//    super.testCheckTxKey();
-//  }
-//
-//  @Override
-//  public void testCheckTxProof() {
-//    super.testCheckTxProof();
-//  }
-//
-//  @Override
-//  public void testCheckSpendProof() {
-//    super.testCheckSpendProof();
-//  }
-//
-//  @Override
-//  public void testGetReserveProofWallet() {
-//    super.testGetReserveProofWallet();
-//  }
-//
-//  @Override
-//  public void testGetReserveProofAccount() {
-//    super.testGetReserveProofAccount();
-//  }
-//
-//  @Override
-//  public void testGetOutputsHex() {
-//    super.testGetOutputsHex();
-//  }
-//
-//  @Override
-//  public void testImportOutputsHex() {
-//    super.testImportOutputsHex();
-//  }
-//
-//  @Override
-//  public void testGetSignedKeyImages() {
-//    super.testGetSignedKeyImages();
-//  }
-//
-//  @Override
-//  public void testGetNewKeyImagesFromLastImport() {
-//    super.testGetNewKeyImagesFromLastImport();
-//  }
-//
-//  @Override
-//  public void testImportKeyImages() {
-//    super.testImportKeyImages();
-//  }
-//
-//  @Override
-//  public void testSignAndVerifyMessages() {
-//    super.testSignAndVerifyMessages();
-//  }
-//
-//  @Override
-//  public void testSetKeyValues() {
-//    super.testSetKeyValues();
-//  }
-//
-//  @Override
-//  public void testCreatePaymentUri() {
-//    super.testCreatePaymentUri();
-//  }
-//
-//  @Override
-//  public void testMining() {
-//    super.testMining();
-//  }
-//
-//  @Override
-//  public void testSendFromSubaddresses() {
-//    super.testSendFromSubaddresses();
-//  }
-//
-//  @Override
-//  public void testSendFromSubaddressesSplit() {
-//    super.testSendFromSubaddressesSplit();
-//  }
-//
-//  @Override
-//  public void testSend() {
-//    super.testSend();
-//  }
-//
-//  @Override
-//  public void testSendWithPaymentId() {
-//    super.testSendWithPaymentId();
-//  }
-//
-//  @Override
-//  public void testSendWithRingSize() {
-//    super.testSendWithRingSize();
-//  }
-//
-//  @Override
-//  public void testSendSplit() {
-//    super.testSendSplit();
-//  }
-//
-//  @Override
-//  public void testCreateThenRelay() {
-//    super.testCreateThenRelay();
-//  }
-//
-//  @Override
-//  public void testCreateThenRelaySplit() {
-//    super.testCreateThenRelaySplit();
-//  }
-//
-//  @Override
-//  public void testSendToMultiple() {
-//    super.testSendToMultiple();
-//  }
-//
-//  @Override
-//  public void testSendToMultipleSplit() {
-//    super.testSendToMultipleSplit();
-//  }
-//
-//  @Override
-//  public void testSendDustToMultipleSplit() {
-//    super.testSendDustToMultipleSplit();
-//  }
-//
-//  @Override
-//  public void testUpdateLockedSameAccount() {
-//    super.testUpdateLockedSameAccount();
-//  }
-//
-//  @Override
-//  public void testUpdateLockedSameAccountSplit() {
-//    super.testUpdateLockedSameAccountSplit();
-//  }
-//
-//  @Override
-//  public void testUpdateLockedDifferentAccounts() {
-//    super.testUpdateLockedDifferentAccounts();
-//  }
-//
-//  @Override
-//  public void testUpdateLockedDifferentAccountsSplit() {
-//    super.testUpdateLockedDifferentAccountsSplit();
-//  }
-//
-//  @Override
-//  public void testSweepOutputs() {
-//    super.testSweepOutputs();
-//  }
-//
-//  @Override
-//  public void testSweepSubaddresses() {
-//    super.testSweepSubaddresses();
-//  }
-//
-//  @Override
-//  public void testSweepAccounts() {
-//    super.testSweepAccounts();
-//  }
-//
-//  @Override
-//  public void testSweepWalletByAccounts() {
-//    super.testSweepWalletByAccounts();
-//  }
-//
-//  @Override
-//  public void testSweepWalletBySubaddresses() {
-//    super.testSweepWalletBySubaddresses();
-//  }
-//
-//  @Override
-//  public void testSweepDustNoRelay() {
-//    super.testSweepDustNoRelay();
-//  }
-//
-//  @Override
-//  public void testSweepDust() {
-//    super.testSweepDust();
-//  }
-//
-//  @Override
-//  public void testRescanBlockchain() {
-//    super.testRescanBlockchain();
-//  }
+  @Override
+  public void testGetHeight() {
+    super.testGetHeight();
+  }
+
+  @Override
+  public void testGetMnemonic() {
+    super.testGetMnemonic();
+  }
+
+  @Override
+  public void testGetSupportedLanguages() {
+    super.testGetSupportedLanguages();
+  }
+
+  @Override
+  public void testGetPrivateViewKey() {
+    super.testGetPrivateViewKey();
+  }
+
+  @Override
+  public void testGetPrimaryAddress() {
+    super.testGetPrimaryAddress();
+  }
+
+  @Override
+  public void testGetIntegratedAddressFromPaymentId() {
+    super.testGetIntegratedAddressFromPaymentId();
+  }
+
+  @Override
+  public void testDecodeIntegratedAddress() {
+    super.testDecodeIntegratedAddress();
+  }
+
+  @Override
+  public void testSyncWithoutProgress() {
+    super.testSyncWithoutProgress();
+  }
+
+  @Override
+  public void testGetAccountsWithoutSubaddresses() {
+    super.testGetAccountsWithoutSubaddresses();
+  }
+
+  @Override
+  public void testGetAccountsWithSubaddresses() {
+    super.testGetAccountsWithSubaddresses();
+  }
+
+  @Override
+  public void testGetAccount() {
+    super.testGetAccount();
+  }
+
+  @Override
+  public void testCreateAccountWithoutLabel() {
+    super.testCreateAccountWithoutLabel();
+  }
+
+  @Override
+  public void testCreateAccountWithLabel() {
+    super.testCreateAccountWithLabel();
+  }
+
+  @Override
+  public void testGetSubaddresses() {
+    super.testGetSubaddresses();
+  }
+
+  @Override
+  public void testGetSubaddressesByIndices() {
+    super.testGetSubaddressesByIndices();
+  }
+
+  @Override
+  public void testGetSubaddressByIndex() {
+    super.testGetSubaddressByIndex();
+  }
+
+  @Override
+  public void testCreateSubaddress() {
+    super.testCreateSubaddress();
+  }
+
+  @Override
+  public void testGetSubaddressAddress() {
+    super.testGetSubaddressAddress();
+  }
+
+  @Override
+  public void testGetAddressIndices() {
+    super.testGetAddressIndices();
+  }
+
+  @Override
+  public void testGetAllBalances() {
+    super.testGetAllBalances();
+  }
+
+  @Override
+  public void testGetTxsWallet() {
+    super.testGetTxsWallet();
+  }
+
+  @Override
+  public void testGetTxsById() {
+    super.testGetTxsById();
+  }
+
+  @Override
+  public void testGetTxsWithConfiguration() {
+    super.testGetTxsWithConfiguration();
+  }
+
+  @Override
+  public void testGetTxsWithPaymentIds() {
+    super.testGetTxsWithPaymentIds();
+  }
+
+  @Override
+  public void testGetTxsFieldsWithFiltering() {
+    super.testGetTxsFieldsWithFiltering();
+  }
+
+  @Override
+  public void testGetTxsValidateInputs() {
+    super.testGetTxsValidateInputs();
+  }
+
+  @Override
+  public void testGetTransfers() {
+    super.testGetTransfers();
+  }
+
+  @Override
+  public void testGetTransfersWithConfiguration() {
+    super.testGetTransfersWithConfiguration();
+  }
+
+  @Override
+  public void testGetTransfersValidateInputs() {
+    super.testGetTransfersValidateInputs();
+  }
+
+  @Override
+  public void testGetOutputs() {
+    super.testGetOutputs();
+  }
+
+  @Override
+  public void testGetOutputsWithConfiguration() {
+    super.testGetOutputsWithConfiguration();
+  }
+
+  @Override
+  public void testGetOutputsValidateInputs() {
+    super.testGetOutputsValidateInputs();
+  }
+
+  @Override
+  public void testAccounting() {
+    super.testAccounting();
+  }
+
+  @Override
+  public void testSetTransactionNote() {
+    super.testSetTransactionNote();
+  }
+
+  @Override
+  public void testSetTransactionNotes() {
+    super.testSetTransactionNotes();
+  }
+
+  @Override
+  public void testCheckTxKey() {
+    super.testCheckTxKey();
+  }
+
+  @Override
+  public void testCheckTxProof() {
+    super.testCheckTxProof();
+  }
+
+  @Override
+  public void testCheckSpendProof() {
+    super.testCheckSpendProof();
+  }
+
+  @Override
+  public void testGetReserveProofWallet() {
+    super.testGetReserveProofWallet();
+  }
+
+  @Override
+  public void testGetReserveProofAccount() {
+    super.testGetReserveProofAccount();
+  }
+
+  @Override
+  public void testGetOutputsHex() {
+    super.testGetOutputsHex();
+  }
+
+  @Override
+  public void testImportOutputsHex() {
+    super.testImportOutputsHex();
+  }
+
+  @Override
+  public void testGetSignedKeyImages() {
+    super.testGetSignedKeyImages();
+  }
+
+  @Override
+  public void testGetNewKeyImagesFromLastImport() {
+    super.testGetNewKeyImagesFromLastImport();
+  }
+
+  @Override
+  public void testImportKeyImages() {
+    super.testImportKeyImages();
+  }
+
+  @Override
+  public void testSignAndVerifyMessages() {
+    super.testSignAndVerifyMessages();
+  }
+
+  @Override
+  public void testSetKeyValues() {
+    super.testSetKeyValues();
+  }
+
+  @Override
+  public void testCreatePaymentUri() {
+    super.testCreatePaymentUri();
+  }
+
+  @Override
+  public void testMining() {
+    super.testMining();
+  }
+
+  @Override
+  public void testSendFromSubaddresses() {
+    super.testSendFromSubaddresses();
+  }
+
+  @Override
+  public void testSendFromSubaddressesSplit() {
+    super.testSendFromSubaddressesSplit();
+  }
+
+  @Override
+  public void testSend() {
+    super.testSend();
+  }
+
+  @Override
+  public void testSendWithPaymentId() {
+    super.testSendWithPaymentId();
+  }
+
+  @Override
+  public void testSendWithRingSize() {
+    super.testSendWithRingSize();
+  }
+
+  @Override
+  public void testSendSplit() {
+    super.testSendSplit();
+  }
+
+  @Override
+  public void testCreateThenRelay() {
+    super.testCreateThenRelay();
+  }
+
+  @Override
+  public void testCreateThenRelaySplit() {
+    super.testCreateThenRelaySplit();
+  }
+
+  @Override
+  public void testSendToMultiple() {
+    super.testSendToMultiple();
+  }
+
+  @Override
+  public void testSendToMultipleSplit() {
+    super.testSendToMultipleSplit();
+  }
+
+  @Override
+  public void testSendDustToMultipleSplit() {
+    super.testSendDustToMultipleSplit();
+  }
+
+  @Override
+  public void testUpdateLockedSameAccount() {
+    super.testUpdateLockedSameAccount();
+  }
+
+  @Override
+  public void testUpdateLockedSameAccountSplit() {
+    super.testUpdateLockedSameAccountSplit();
+  }
+
+  @Override
+  public void testUpdateLockedDifferentAccounts() {
+    super.testUpdateLockedDifferentAccounts();
+  }
+
+  @Override
+  public void testUpdateLockedDifferentAccountsSplit() {
+    super.testUpdateLockedDifferentAccountsSplit();
+  }
+
+  @Override
+  public void testSweepOutputs() {
+    super.testSweepOutputs();
+  }
+
+  @Override
+  public void testSweepSubaddresses() {
+    super.testSweepSubaddresses();
+  }
+
+  @Override
+  public void testSweepAccounts() {
+    super.testSweepAccounts();
+  }
+
+  @Override
+  public void testSweepWalletByAccounts() {
+    super.testSweepWalletByAccounts();
+  }
+
+  @Override
+  public void testSweepWalletBySubaddresses() {
+    super.testSweepWalletBySubaddresses();
+  }
+
+  @Override
+  public void testSweepDustNoRelay() {
+    super.testSweepDustNoRelay();
+  }
+
+  @Override
+  public void testSweepDust() {
+    super.testSweepDust();
+  }
+
+  @Override
+  public void testRescanBlockchain() {
+    super.testRescanBlockchain();
+  }
 }
