3be2ba0e6a5442429de2f1e3d297797b03740a62
woodser <woodser@protonmail.com>
2019-07-18 07:45:55 -0400

    testGetTxsWallet() asserts that first tx happens on configured test wallet restore height; implement precise tests for start height vs restore height
    Former-commit-id: 47d92fbb155d862d916a3051f8d5916b9c3f22a9

--- src/test/java/test/TestMoneroWalletCommon.java
+++ src/test/java/test/TestMoneroWalletCommon.java
@@ -468,6 +468,8 @@ public abstract class TestMoneroWalletCommon extends TestMoneroBase {
     List<MoneroTxWallet> txs1 = getCachedTxs();
     List<MoneroTxWallet> txs2 = getAndTestTxs(wallet, null, null, true);
     assertEquals(txs2.size(), txs1.size());
+    assertFalse("Wallet has no txs to test", txs1.isEmpty());
+    assertEquals("First tx's restore height must match the restore height in TestUtils", TestUtils.TEST_RESTORE_HEIGHT, (long) txs1.get(0).getHeight());  // TODO: port this to js
     
     // build test context
     TestContext ctx = new TestContext();
