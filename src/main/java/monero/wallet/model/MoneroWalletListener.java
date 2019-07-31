477fb95690de89fae5a6310400c2055301ed6746
woodser <woodser@protonmail.com>
2019-07-31 12:46:52 -0400

    rename to MoneroWalletListenerI and MoneroWalletListener
    Former-commit-id: 2687bcfa6e7c91cd1ee949caa68aa8635a83043d

--- src/main/java/monero/wallet/model/MoneroWalletListener.java
+++ src/main/java/monero/wallet/model/MoneroWalletListener.java
@@ -1,37 +1,22 @@
 package monero.wallet.model;
 
 /**
- * Interface to receive wallet notifications.
+ * Provides default handling for wallet notifications.
  */
-public interface MoneroWalletListener extends MoneroSyncListener {
-  
-  /**
-   * Invoked when a new block is added to the chain.
-   * 
-   * @param height is the height of the block added to the chain
-   */
-  public void onNewBlock(long height);
-  
-  /**
-   * Invoked when the wallet observes a transaction with incoming or outgoing transfers
-   * to or from the wallet.
-   * 
-   * @param tx is the transaction with incoming or outgoing transfers to or from the wallet
-   */
-  public void onNewTx(MoneroTxWallet tx);
-  
-  /**
-   * Invoked when the wallet receives one or more incoming transfers as part of
-   * a transaction.
-   * 
-   * @param tx is the transaction with incoming transfers
-   */
-  public void onMoneyReceived(MoneroTxWallet tx);
-  
-  /**
-   * Invoked when the wallet sends an outgoing transfer.
-   * 
-   * @param tx is the transaction with an outgoing transfer
-   */
-  public void onMoneySent(MoneroTxWallet tx);
+public class MoneroWalletListener implements MoneroWalletListenerI {
+
+  @Override
+  public void onSyncProgress(long height, long startHeight, long endHeight, double percentDone, String message) { }
+
+  @Override
+  public void onNewBlock(long height) { }
+
+  @Override
+  public void onNewTx(MoneroTxWallet tx) { }
+
+  @Override
+  public void onMoneyReceived(MoneroTxWallet tx) { }
+
+  @Override
+  public void onMoneySent(MoneroTxWallet tx) { }
 }
