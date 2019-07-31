8f963a924c30034522d4d4ca79ad279f1e3c3448
woodser <woodser@protonmail.com>
2019-07-30 21:55:30 -0400

    defined wallet listener interface onNewTx(), onMoneyReceived(), onMoneySent(); testing for invocation of these methods on wallet sync completion
    Former-commit-id: 6b40b4de807dd96da59de72e1b5277dd19cc042e

--- src/main/java/monero/wallet/model/MoneroWalletListener.java
+++ src/main/java/monero/wallet/model/MoneroWalletListener.java
@@ -7,5 +7,33 @@ import monero.daemon.model.MoneroBlockHeader;
  */
 public interface MoneroWalletListener extends MoneroSyncListener {
   
+  /**
+   * Invoked when a new block is added to the chain.
+   * 
+   * @param header is the header of the block added to the chain
+   */
   public void onNewBlock(MoneroBlockHeader header);
+  
+  /**
+   * Invoked when the wallet observes a transaction with incoming or outgoing transfers
+   * to or from the wallet.
+   * 
+   * @param tx is the transaction with incoming or outgoing transfers to or from the wallet
+   */
+  public void onNewTx(MoneroTxWallet tx);
+  
+  /**
+   * Invoked when the wallet receives one or more incoming transfers as part of
+   * a transaction.
+   * 
+   * @param tx is the transaction with incoming transfers
+   */
+  public void onMoneyReceived(MoneroTxWallet tx);
+  
+  /**
+   * Invoked when the wallet sends an outgoing transfer.
+   * 
+   * @param tx is the transaction with an outgoing transfer
+   */
+  public void onMoneySent(MoneroTxWallet tx);
 }
