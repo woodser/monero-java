ca1bd1115c6a143b14918fcf61e1b727a9926895
woodser <woodser@protonmail.com>
2019-08-23 17:12:17 -0400

    mine with 7->6 threads
    Former-commit-id: 8add4dc7dc2840c77764e5b8173f202328830d1f

--- src/test/java/utils/StartMining.java
+++ src/test/java/utils/StartMining.java
@@ -7,7 +7,7 @@ public class StartMining {
   }
   
   public static void startMining() {
-    int numThreads = 7;
+    int numThreads = 6;
     //TestUtils.getWalletRpc().startMining(numThreads, false, true);
     TestUtils.getDaemonRpc().startMining("56SWsnhejUTbgNs2EgyXdfNXUawymMMuAC9voZZSQrHzJHNxGsAvMnoUja7JcKVtPwNc1oKAkoAt1cv6EmtKRQ22U37B7cT", numThreads, false, false);  // random subaddress
   }
