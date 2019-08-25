c537002b8e5ca930460b68d2cce6c40958791f66
woodser <woodser@protonmail.com>
2019-08-25 08:32:45 -0400

    add scratchpad script to fund 10 test wallets

--- src/test/java/utils/Scratchpad.java
+++ src/test/java/utils/Scratchpad.java
@@ -1,8 +1,20 @@
 package utils;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import monero.daemon.MoneroDaemon;
+import monero.daemon.model.MoneroNetworkType;
+import monero.utils.MoneroUtils;
+import monero.wallet.MoneroWallet;
+import monero.wallet.MoneroWalletJni;
+import monero.wallet.MoneroWalletRpc;
+import monero.wallet.model.MoneroDestination;
+import monero.wallet.model.MoneroSendRequest;
+import monero.wallet.model.MoneroTxSet;
 import monero.wallet.model.MoneroTxWallet;
 
 /**
@@ -15,8 +27,8 @@ public class Scratchpad {
     
     // initialize daemon, wallet, and direct rpc interface
     MoneroDaemon daemon = TestUtils.getDaemonRpc();
-    //MoneroWalletRpc walletRpc = TestUtils.getWalletRpc();
-    //MoneroWalletJni walletJni = TestUtils.getWalletJni();
+    MoneroWalletRpc walletRpc = TestUtils.getWalletRpc();
+    MoneroWalletJni walletJni = TestUtils.getWalletJni();
     //MoneroRpc rpc = new MoneroRpc(TestUtils.WALLET_RPC_CONFIG);
     
 //    // common variables
@@ -26,6 +38,8 @@ public class Scratchpad {
     String txId = null;
     
     // -------------------------------- SCRATCHPAD ----------------------------
+    
+    
     
     // MEASURE LAST 30 DAYS
 //    int numBlocks = 30 * 24 * 60 / 2;
@@ -75,5 +89,38 @@ public class Scratchpad {
 //      System.out.println(mnemonics.get(i));
 //      System.out.println(addresses.get(i));
 //    }
+    
+    // create and fund 10 wallets
+    TestUtils.TX_POOL_WALLET_TRACKER.waitForWalletTxsToClearPool(walletJni);
+    for (int i = 0; i < 10; i++) {
+      
+      // create wallet and create and collect subaddresses
+      List<String> subaddresses = new ArrayList<String>();
+      String path = TestUtils.TEST_WALLETS_DIR + "/funded_" + UUID.randomUUID().toString();
+      MoneroWalletJni fundedWallet = MoneroWalletJni.createWalletRandom(path, TestUtils.WALLET_PASSWORD, MoneroNetworkType.STAGENET);
+      for (int accountIdx = 0; accountIdx < 3; accountIdx++) {
+        fundedWallet.createAccount();
+        for (int subaddressIdx = 0; subaddressIdx < 5; subaddressIdx++) {
+          fundedWallet.createSubaddress(accountIdx);
+          subaddresses.add(fundedWallet.getAddress(accountIdx, subaddressIdx));
+        }
+      }
+      fundedWallet.close(true);
+      
+      // wait for unlocked funds
+      while (walletJni.getUnlockedBalance(0).compareTo(TestUtils.MAX_FEE) < 0) {
+        System.out.println("Waiting...");
+        try { TimeUnit.MILLISECONDS.sleep(MoneroUtils.WALLET2_REFRESH_INTERVAL); }
+        catch (InterruptedException e) { throw new RuntimeException(e.getMessage()); }
+      }
+      
+      // transfer funds to subaddresses
+      List<MoneroDestination> destinations = new ArrayList<MoneroDestination>();
+      for (String address : subaddresses) destinations.add(new MoneroDestination(address, TestUtils.MAX_FEE.multiply(BigInteger.valueOf(2))));
+      System.out.println("Transferring....");
+      MoneroTxSet txSet = walletJni.sendSplit(new MoneroSendRequest().setDestinations(destinations).setAccountIndex(0));
+      System.out.println("Tx set has " + txSet.getTxs().size() + " transactions");
+      for (MoneroTxWallet tx : txSet.getTxs()) System.out.println(tx);
+    }
   }
 }
