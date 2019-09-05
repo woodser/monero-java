88c5480ff34e62e4cc30ea58da8893c752ec3de9
woodser <woodser@protonmail.com>
2019-09-05 11:23:43 -0400

    cleanup

--- src/test/java/utils/FundWallets.java
+++ src/test/java/utils/FundWallets.java
@@ -23,7 +23,7 @@ public class FundWallets {
   
   public static final int NUM_WALLETS = 10;
   public static final int NUM_ACCOUNTS = 5;
-  public static final int NUM_SUBADDRESSES_PER_ACCOUNT = 3; // num accounts x num subaddresses must be <= 16 per protocol restriction // TODO: could be made 
+  public static final int NUM_SUBADDRESSES_PER_ACCOUNT = 3; 
   
   public static void main(String[] args) {
     fundWallets(TestUtils.getWalletJni(), NUM_WALLETS, NUM_ACCOUNTS, NUM_SUBADDRESSES_PER_ACCOUNT);
@@ -35,7 +35,7 @@ public class FundWallets {
    * @param srcWallet is the source wallet to fund test wallets with
    * @param numWallets is the number of wallets to fund
    * @param numAccounts is the number of accounts to fund in each test wallet
-   * @param numSubaddressesPerAccount is the number of subaddresses to fund in each account
+   * @param numSubaddressesPerAccount is the number of subaddresses to fund in each account (num accounts * num subaddresses must be <= 16) // TODO: support funding more subaddresses
    */
   public static void fundWallets(MoneroWallet srcWallet, int numWallets, int numAccounts, int numSubaddressesPerAccount) {
     TestUtils.TX_POOL_WALLET_TRACKER.waitForWalletTxsToClearPool(srcWallet);
