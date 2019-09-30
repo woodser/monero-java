8658b5a9b57dd6c8d92b83d80a7d17fe61edbe7a
woodser <woodser@protonmail.com>
2019-09-30 07:35:16 -0400

    re-add GenUtils.subarray which defaults to empty array
    Former-commit-id: 5607fffbe94a3a9dc29870c07422ec997b8d5bf9

--- src/main/java/common/utils/GenUtils.java
+++ src/main/java/common/utils/GenUtils.java
@@ -8,7 +8,7 @@ import java.util.List;
  */
 public class GenUtils {
   
-  public static final int[] EMPTY_INT_ARRAY = new int[0];
+  private static final int[] EMPTY_INT_ARRAY = new int[0];
   
   /**
    * Asserts that the given boolean is true.
@@ -177,25 +177,17 @@ public class GenUtils {
   }
   
   /**
-   * Produces a new array containing the elements between
-   * the start and end indices.
+   * Produces a new array containing the elements between the start and end indices.
    * <p>
    * Code from <a href="https://commons.apache.org/proper/commons-lang/">Apache Commons Lang</a>.
    */
   public static int[] subarray(final int[] array, int startIndexInclusive, int endIndexExclusive) {
-    if (array == null) {
-        return null;
-    }
-    if (startIndexInclusive < 0) {
-        startIndexInclusive = 0;
-    }
-    if (endIndexExclusive > array.length) {
-        endIndexExclusive = array.length;
-    }
+    if (array == null) return null;
+    if (startIndexInclusive < 0) startIndexInclusive = 0;
+    if (endIndexExclusive > array.length) endIndexExclusive = array.length;
+    
     final int newSize = endIndexExclusive - startIndexInclusive;
-    if (newSize <= 0) {
-        return EMPTY_INT_ARRAY;
-    }
+    if (newSize <= 0) return EMPTY_INT_ARRAY;
 
     final int[] subarray = new int[newSize];
     System.arraycopy(array, startIndexInclusive, subarray, 0, newSize);
