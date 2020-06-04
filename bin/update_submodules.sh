30fefb48ec53549ef0f15ebb9b846f48030b57fe
woodser <woodser@protonmail.com>
2020-06-04 13:49:11 -0400

    checkout tags

--- bin/update_submodules.sh
+++ bin/update_submodules.sh
@@ -4,16 +4,16 @@
 git submodule update --init --recursive
 
 # update monero-java
-git checkout v0.3.0
+git checkout tags/v0.3.0
 git pull --ff-only origin master
 
 # update monero-cpp
 cd ./external/monero-cpp
-git checkout v0.3.0
+git checkout tags/v0.3.0
 git pull --ff-only origin master
 
 # update monero-core
 cd ./external/monero-core
-git checkout v0.16.0.0
+git checkout tags/v0.16.0.0
 git pull --ff-only origin master
 cd ../../../../
