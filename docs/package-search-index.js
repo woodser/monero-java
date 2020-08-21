48b337a1ea5d0b227217dc2abf7b60554022677b
woodser <woodser@protonmail.com>
2020-08-21 12:14:28 -0400

    update javadoc

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"common.types"},{"l":"common.utils"},{"l":"monero.common"},{"l":"monero.daemon"},{"l":"monero.daemon.model"},{"l":"monero.wallet"},{"l":"monero.wallet.model"}]
