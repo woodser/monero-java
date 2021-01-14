892c837730babf4132e71e5460fada82ca517932
woodser <woodser@protonmail.com>
2021-01-14 09:51:00 -0500

    StartMining supports numThreads

--- src/test/java/utils/StartMining.java
+++ src/test/java/utils/StartMining.java
@@ -10,7 +10,10 @@ public class StartMining {
   }
   
   public static void startMining() {
-    long numThreads = 1;
+    startMining(1);
+  }
+  
+  public static void startMining(long numThreads) {
     //TestUtils.getWalletRpc().startMining(numThreads, false, true);
     TestUtils.getDaemonRpc().startMining("59dF9pSotECe1Fn4dBGZXWHYyNdo53rbZ7YYseu9jBKCf4c2cUzhuFVRH8HuD4wyaKTqtD3VF3F4eQe3Kzq342F5U8R4jeq", numThreads, false, false);
   }
