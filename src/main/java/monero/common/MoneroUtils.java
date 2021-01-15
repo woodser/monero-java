7cd10ebce031fa1eeb4334d920b90400cc8df994
woodser <woodser@protonmail.com>
2021-01-15 13:47:56 -0500

    bump version to v0.5.0

--- src/main/java/monero/common/MoneroUtils.java
+++ src/main/java/monero/common/MoneroUtils.java
@@ -39,7 +39,7 @@ public class MoneroUtils {
    * @return the version of this monero-java library
    */
   static String getVersion() {
-    return "0.3.7";
+    return "0.5.0";
   }
   
   public static final int RING_SIZE = 12; // network-enforced ring size
