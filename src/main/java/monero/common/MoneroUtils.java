932071c809aa5a853ce0f0b6765579fbf06eda3b
woodser <woodser@protonmail.com>
2024-04-01 11:13:43 -0400

    bump version to 0.8.17

--- src/main/java/monero/common/MoneroUtils.java
+++ src/main/java/monero/common/MoneroUtils.java
@@ -36,7 +36,7 @@ public class MoneroUtils {
    * @return the version of this monero-java library
    */
   public static String getVersion() {
-    return "0.8.16";
+    return "0.8.17";
   }
 
   // try to load jni bindings
