d176d3fdd0cd7d835bf919030839807bae3e3115
woodser <13068859+woodser@users.noreply.github.com>
2025-06-12 08:58:12 -0400

    update javadocs

--- docs/javadocs/package-search-index.js
+++ docs/javadocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"common.types"},{"l":"common.utils"},{"l":"monero.common"},{"l":"monero.daemon"},{"l":"monero.daemon.model"},{"l":"monero.wallet"},{"l":"monero.wallet.model"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"common.types"},{"l":"common.utils"},{"l":"monero.common"},{"l":"monero.daemon"},{"l":"monero.daemon.model"},{"l":"monero.wallet"},{"l":"monero.wallet.model"},{"l":"utils"}];updateSearchResults();
