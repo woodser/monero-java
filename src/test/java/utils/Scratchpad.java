7b119deaaa372344087776dc949c589c4f6c317e
woodser <13068859+woodser@users.noreply.github.com>
2026-08-26 14:59:03 -0400

    test: update scratchpad to create mainnet wallet from seed

--- src/test/java/utils/Scratchpad.java
+++ src/test/java/utils/Scratchpad.java
@@ -2,10 +2,13 @@ package utils;
 
 import java.util.UUID;
 
+import monero.daemon.MoneroDaemon;
+import monero.daemon.MoneroDaemonRpc;
 import monero.daemon.model.MoneroNetworkType;
 import monero.wallet.MoneroWallet;
 import monero.wallet.MoneroWalletFull;
 import monero.wallet.model.MoneroWalletConfig;
+import monero.wallet.model.MoneroWalletListener;
 
 /**
  * Scratchpad for quick scripting.
@@ -13,28 +16,33 @@ import monero.wallet.model.MoneroWalletConfig;
 public class Scratchpad {
 
   public static void main(String[] args) {
-    
+
     // initialize daemon, wallet, and direct rpc interface
 //    MoneroDaemon daemon = TestUtils.getDaemonRpc();
 //    MoneroWalletRpc walletRpc = TestUtils.getWalletRpc();
 //    MoneroWalletFull walletFull = TestUtils.getWalletFull();
-    
+
     // -------------------------------- SCRATCHPAD ----------------------------
-    
-    // create wallet from seed
+
+    // create wallet from seed on mainnet
+    MoneroDaemon daemon = new MoneroDaemonRpc("http://xmr-node.cakewallet.com:18081");
     MoneroWallet walletFull = MoneroWalletFull.createWallet(new MoneroWalletConfig()
       .setPath("./test_wallets/" + UUID.randomUUID().toString())  // leave blank for in-memory wallet
       .setPassword("abctesting123")
-      .setNetworkType(MoneroNetworkType.TESTNET)
-      .setServerUri("http://localhost:38081")
-      .setServerUsername("superuser")
-      .setServerPassword("abctesting123")
-      .setSeed("biggest duets beware eskimos coexist igloo pamphlet lagoon odometer hounded jukebox enough pride cocoa nylon wolf geometry buzzer vivid federal idols gang semifinal subtly coexist")
-      .setRestoreHeight(573800l));
-    walletFull.sync(new WalletSyncPrinter());
+      .setNetworkType(MoneroNetworkType.MAINNET)
+      .setServerUri("http://xmr-node.cakewallet.com:18081")
+      //.setServerProxyUri("127.0.0.1:59787")
+      .setRestoreHeight(daemon.getHeight() - 1000)
+      .setSeed("inbound boldly fuselage jukebox unveil rounded village summon swiftly aside shuffled rising examine friendly goat rockets girth mugged january yesterday went dented amnesty awful unveil"));
+    walletFull.sync(new MoneroWalletListener() {
+      @Override
+      public void onSyncProgress(long height, long startHeight, long endHeight, double percentDone, String message) {
+        System.out.println("Sync progress: " + percentDone + "%, height: " + height + ", startHeight: " + startHeight + ", endHeight: " + endHeight + ", message: " + message);
+      }
+    });
     System.out.println("Full wallet daemon height: " + walletFull.getDaemonHeight());
     System.out.println("Full wallet seed: " + walletFull.getSeed());
-    
+
 //    walletFull.createTx(new MoneroTxConfig()
 //            .addDestination("52FnB7ABUrKJzVQRpbMNrqDFWbcKLjFUq8Rgek7jZEuB6WE2ZggXaTf4FK6H8gQymvSrruHHrEuKhMN3qTMiBYzREKsmRKM", walletFull.getUnlockedBalance(0).divide(new BigInteger("8")).multiply(new BigInteger("1")))
 //            .addDestination("52aPELZwrwvVBNK4pvRZPNj4U5EEkZBsNTR2jozCLYyrhQySvYbWebTQEdt7RS9nFnRY9r88eFpt6UcsHKnVpCQDAFKu1Az", walletFull.getUnlockedBalance(0).divide(new BigInteger("8")).multiply(new BigInteger("1")))
